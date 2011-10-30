b85eaa72d03ad6aea548ef8ddb2b3649e84c9ec2
dpanozzo <devnull@localhost>
2011-10-30 13:08:18 +0000

    fixes to df_query and df_build

--- mesh/df_build.m
+++ mesh/df_build.m
@@ -1,34 +1,45 @@
-function [DF] = df_build(V,F,C)
-    
-    if ~exist('C','var')
-        C=[10;10;10];
-    end
-    
-    MIN = min(V,[],1);
-    MAX = max(V,[],1);
+function [DF] = df_build(V,C)
+% [DF] = df_build(V,C)
+% Build a volumetric distance field and spatial index that can be used to
+% speed up approximate NN queries in 3D
+% This code is not optimized, it is very slow for big datasets
+%
+% Input:
+% V: Coordinates of the points
+% C: Column vector that defines the number of cells for every
+%    dimension
+% Output:
+% DF: structure to be passed to df_query to execute queries
 
-    S = (MAX-MIN)./C';
-    
-    D = zeros(C'+1);
-    N = zeros(C'+1);
-    
-    for x=1:size(D,1)
-        progressbar(x,size(D,1));
-        for y=1:size(D,2)
-            for z=1:size(D,3)
-                i = [x,y,z];
-                p = MIN + (i-1).*S;
-                [D(x,y,z) N(x,y,z)] = min(normrow(repmat(p,size(V,1),1)-V));
-            end
+if ~exist('C','var')
+    C=[10;10;10];
+end
+
+MIN = min(V,[],1);
+MAX = max(V,[],1);
+
+S = (MAX-MIN)./C';
+
+D = zeros(C'+1);
+N = zeros(C'+1);
+
+for x=1:size(D,1)
+    progressbar(x,size(D,1));
+    for y=1:size(D,2)
+        for z=1:size(D,3)
+            i = [x,y,z];
+            p = MIN + (i-1).*S;
+            [D(x,y,z) N(x,y,z)] = min(normrow(repmat(p,size(V,1),1)-V));
         end
     end
-    
-    
-    % Prepare DF struct
-    DF.MIN = MIN;
-    DF.MAX = MAX;
-    DF.D = D;
-    DF.N = N;
-    DF.C = C;
-    DF.S = S;
+end
+
+
+% Prepare DF struct
+DF.MIN = MIN;
+DF.MAX = MAX;
+DF.D = D;
+DF.N = N;
+DF.C = C;
+DF.S = S;
 end
--- mesh/df_query.m
+++ mesh/df_query.m
@@ -1,8 +1,22 @@
-function [id] = df_query( DF, p)
-    p = p-DF.MIN;
-    p = p./DF.S;
-    p = round(p)+1;
-    
-    id = DF.N(p(1),p(2),p(3));
+function [id,d] = df_query( DF, p)
+% [id,d] = df_query(DF,p)
+% Use the spatial index build with df_build to provide approximate
+% NN and distance from the NN.
+%
+% Input:
+% DF: output of df_build
+% p: query point
+%
+% Output:
+% id: index of the point closer to p (approximate)
+% d : distance from the point closer to p (approximate)
+
+p = p-DF.MIN;
+p = p./DF.S;
+p = round(p)+1;
+
+id = DF.N(p(1),p(2),p(3));
+d  = DF.D(p(1),p(2),p(3));
+
 end
 
