1d6e6949dac94cb0182a03be8ef2a7ed3cc08dfe
Alec Jacobson (jalec <Alec Jacobson (jalec@inf.ethz.ch)>
2012-11-11 16:29:57 +0100

    fit rigid transformation to two mesh points

---
+++ mesh/fit_rigid.m
@@ -0,0 +1,27 @@
+function [R,t] = fit_rigid(V,U)
+  % FIT_RIGID fit a rigid transformation that best matches V to U in the least
+  % squares sense:
+  %   min ∑ || vi - R(ui+t) ||²
+  %
+  % Inputs:
+  %   V  #V by dim list of points
+  %   U  #V by dim list of points
+  % Outputs:
+  %   R  dim by dim rotation matrix transposed
+  %   t  dim translation vector
+  %
+  % Example:
+  %   [R,t] = fit_rigid(V,U);
+  %   UU = bsxfun(@plus,U,t)*R;
+
+  % translation is just difference of center of mass (assuming uniform mass
+  % distribution)
+  t = mean(V)-mean(U);
+
+  % sum up outerproducts of vector from each vertex to center of mass in U and
+  % V
+  S = bsxfun(@minus,U,mean(U))'*bsxfun(@minus,V,mean(V));
+  % Find closest rotation
+  R = fit_rotation(S');
+
+end
