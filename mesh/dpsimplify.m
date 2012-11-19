8bc79e037588b54c91d7e126bac1d382f38825f9
Alec Jacobson (jalec <Alec Jacobson (jalec@inf.ethz.ch)>
2012-11-19 11:47:05 +0100

    adding get_pencil and dp from inflate

---
+++ mesh/dpsimplify.m
@@ -0,0 +1,244 @@
+function [ps,ix] = dpsimplify(p,tol)
+
+% Recursive Douglas-Peucker Polyline Simplification, Simplify
+%
+% [ps,ix] = dpsimplify(p,tol)
+%
+% dpsimplify uses the recursive Douglas-Peucker line simplification 
+% algorithm to reduce the number of vertices in a piecewise linear curve 
+% according to a specified tolerance. The algorithm is also know as
+% Iterative Endpoint Fit. It works also for polylines and polygons
+% in higher dimensions.
+%
+% In case of nans (missing vertex coordinates) dpsimplify assumes that 
+% nans separate polylines. As such, dpsimplify treats each line
+% separately.
+%
+% For additional information on the algorithm follow this link
+% http://en.wikipedia.org/wiki/Ramer-Douglas-Peucker_algorithm
+%
+% Input arguments
+%
+%     p     polyline n*d matrix with n vertices in d 
+%           dimensions.
+%     tol   tolerance (maximal euclidean distance allowed 
+%           between the new line and a vertex)
+%
+% Output arguments
+%
+%     ps    simplified line
+%     ix    linear index of the vertices retained in p (ps = p(ix))
+%
+% Examples
+%
+% 1. Simplify line 
+%
+%     tol    = 1;
+%     x      = 1:0.1:8*pi;
+%     y      = sin(x) + randn(size(x))*0.1;
+%     p      = [x' y'];
+%     ps     = dpsimplify(p,tol);
+%
+%     plot(p(:,1),p(:,2),'k')
+%     hold on
+%     plot(ps(:,1),ps(:,2),'r','LineWidth',2);
+%     legend('original polyline','simplified')
+%
+% 2. Reduce polyline so that only knickpoints remain by 
+%    choosing a very low tolerance
+%
+%     p = [(1:10)' [1 2 3 2 4 6 7 8 5 2]'];
+%     p2 = dpsimplify(p,eps);
+%     plot(p(:,1),p(:,2),'k+--')
+%     hold on
+%     plot(p2(:,1),p2(:,2),'ro','MarkerSize',10);
+%     legend('original line','knickpoints')
+%
+% 3. Simplify a 3d-curve
+% 
+%     x = sin(1:0.01:20)'; 
+%     y = cos(1:0.01:20)'; 
+%     z = x.*y.*(1:0.01:20)';
+%     ps = dpsimplify([x y z],0.1);
+%     plot3(x,y,z);
+%     hold on
+%     plot3(ps(:,1),ps(:,2),ps(:,3),'k*-');
+%
+%
+%
+% Author: Wolfgang Schwanghart, 13. July, 2010.
+% w.schwanghart[at]unibas.ch
+
+
+if nargin == 0
+    help dpsimplify
+    return
+end
+
+error(nargchk(2, 2, nargin))
+
+% error checking
+if ~isscalar(tol) || tol<0;
+    error('tol must be a positive scalar')
+end
+
+
+% nr of dimensions
+nrvertices    = size(p,1); 
+dims    = size(p,2);
+
+% anonymous function for starting point and end point comparision
+% using a relative tolerance test
+compare = @(a,b) abs(a-b)/max(abs(a),abs(b)) <= eps;
+
+% what happens, when there are NaNs?
+% NaNs divide polylines.
+Inan      = any(isnan(p),2);
+% any NaN at all?
+Inanp     = any(Inan);
+
+% if there is only one vertex
+if nrvertices == 1 || isempty(p);
+    ps = p;
+    ix = 1;
+
+% if there are two 
+elseif nrvertices == 2 && ~Inanp;
+    % when the line has no vertices (except end and start point of the
+    % line) check if the distance between both is less than the tolerance.
+    % If so, return the center.
+    if dims == 2;
+        d    = hypot(p(1,1)-p(2,1),p(1,2)-p(2,2));
+    else
+        d    = sqrt(sum((p(1,:)-p(2,:)).^2));
+    end
+    
+    if d <= tol;
+        ps = sum(p,1)/2;
+        ix = 1;
+    else
+        ps = p;
+        ix = [1;2];
+    end
+    
+elseif Inanp;
+    
+    % case: there are nans in the p array
+    % --> find start and end indices of contiguous non-nan data
+    Inan = ~Inan;
+    sIX = strfind(Inan',[0 1])' + 1; 
+    eIX = strfind(Inan',[1 0])'; 
+ 
+    if Inan(end)==true;
+        eIX = [eIX;nrvertices];
+    end
+    
+    if Inan(1);
+        sIX = [1;sIX];
+    end
+    
+    % calculate length of non-nan components
+    lIX = eIX-sIX+1;   
+    % put each component into a single cell
+    c   = mat2cell(p(Inan,:),lIX,dims);
+    
+    % now call dpsimplify again inside cellfun. 
+    if nargout == 2;
+        [ps,ix]   = cellfun(@(x) dpsimplify(x,tol),c,'uniformoutput',false);
+        ix        = cellfun(@(x,six) x+six-1,ix,num2cell(sIX),'uniformoutput',false);
+    else
+        ps   = cellfun(@(x) dpsimplify(x,tol),c,'uniformoutput',false);
+    end
+    
+    % write the data from a cell array back to a matrix
+    ps = cellfun(@(x) [x;nan(1,dims)],ps,'uniformoutput',false);    
+    ps = cell2mat(ps);
+    ps(end,:) = [];
+    
+    % ix wanted? write ix to a matrix, too.
+    if nargout == 2;
+        ix = cell2mat(ix);
+    end
+    
+       
+else
+    
+
+% if there are no nans than start the recursive algorithm
+ixe     = size(p,1);
+ixs     = 1;
+
+% logical vector for the vertices to be retained
+I   = true(ixe,1);
+
+% call recursive function
+p   = simplifyrec(p,tol,ixs,ixe);
+ps  = p(I,:);
+
+% if desired return the index of retained vertices
+if nargout == 2;
+    ix  = find(I);
+end
+
+end
+
+% _________________________________________________________
+function p  = simplifyrec(p,tol,ixs,ixe)
+    
+    % check if startpoint and endpoint are the same 
+    % better comparison needed which included a tolerance eps
+    
+    c1 = num2cell(p(ixs,:));
+    c2 = num2cell(p(ixe,:));   
+    
+    % same start and endpoint with tolerance
+    sameSE = all(cell2mat(cellfun(compare,c1(:),c2(:),'UniformOutput',false)));
+
+    
+    if sameSE; 
+        % calculate the shortest distance of all vertices between ixs and
+        % ixe to ixs only
+        if dims == 2;
+            d    = hypot(p(ixs,1)-p(ixs+1:ixe-1,1),p(ixs,2)-p(ixs+1:ixe-1,2));
+        else
+            d    = sqrt(sum(bsxfun(@minus,p(ixs,:),p(ixs+1:ixe-1,:)).^2,2));
+        end
+    else    
+        % calculate shortest distance of all points to the line from ixs to ixe
+        % subtract starting point from other locations
+        pt = bsxfun(@minus,p(ixs+1:ixe,:),p(ixs,:));
+
+        % end point
+        a = pt(end,:)';
+
+        beta = (a' * pt')./(a'*a);
+        b    = pt-bsxfun(@times,beta,a)';
+        if dims == 2;
+            % if line in 2D use the numerical more robust hypot function
+            d    = hypot(b(:,1),b(:,2));
+        else
+            d    = sqrt(sum(b.^2,2));
+        end
+    end
+    
+    % identify maximum distance and get the linear index of its location
+    [dmax,ixc] = max(d);
+    ixc  = ixs + ixc; 
+    
+    % if the maximum distance is smaller than the tolerance remove vertices
+    % between ixs and ixe
+    if dmax <= tol;
+        if ixs ~= ixe-1;
+            I(ixs+1:ixe-1) = false;
+        end
+    % if not, call simplifyrec for the segments between ixs and ixc (ixc
+    % and ixe)
+    else   
+        p   = simplifyrec(p,tol,ixs,ixc);
+        p   = simplifyrec(p,tol,ixc,ixe);
+
+    end
+
+end
+end
+
---
+++ mesh/get_pencil_curve.m
@@ -0,0 +1,98 @@
+function P = get_pencil_curve(f)
+  % GET_PENCIL_CURVE Get a curve (sequence of points) from the user by dragging
+  % on the current plot window
+  %
+  % P = get_pencil_curve()
+  % P = get_pencil_curve(f)
+  % 
+  % Inputs:
+  %   f  figure id
+  % Outputs:
+  %   P  #P by 3 list of points
+  %
+  %
+  P = [];
+
+  % Get the input figure or get current one (creates new one if none exist)
+  if nargin == 0 || isempty(f)
+    f = gcf;
+  end
+  figure(f);
+
+  % get axes of current figure (creates on if doesn't exist)
+  a = gca;
+
+  % set equal axis 
+  axis equal;
+  % freeze axis
+  axis manual;
+  % set view to XY plane
+  view(2);
+
+  rotate3d off;
+  set(gcf,'windowbuttondownfcn',@ondown);
+  set(gcf,'keypressfcn',        @onkeypress);
+  % living variables
+  p = [];
+
+  done = false;
+  while(~done)
+    drawnow;
+  end
+
+  % We've been also gathering Z coordinate which is meaningless
+  P = P(:,1:2);
+
+  % Callback for mouse press
+  function ondown(src,ev)
+    % Tell window that we'll handle drag and up events
+    set(gcf,'windowbuttonmotionfcn', @ondrag);
+    set(gcf,'windowbuttonupfcn',     @onup);
+    append_current_point();
+  end
+
+  % Callback for mouse press
+  function ondrag(src,ev)
+    append_current_point();
+  end
+
+  % Callback for mouse release
+  function onup(src,ev)
+    % Tell window to handle down, drag and up events itself
+    finish();
+  end
+
+  function onkeypress(src,ev)
+    % escape character id
+    ESC = char(27);
+    switch ev.Character
+    case ESC
+      finish();
+    otherwise
+      finish();
+      error(['Unknown key: ' ev.Character]);
+    end
+  end
+
+  function append_current_point()
+    % get current mouse position, and remember old one
+    cp = get(gca,'currentpoint');
+    P = [P;cp(1,:)];
+    if isempty(p)
+      hold on;
+      p = plot(P(:,1),P(:,2));
+      hold off;
+    else
+      set(p,'Xdata',P(:,1),'Ydata',P(:,2));
+    end
+  end
+
+  function finish()
+    done = true;
+    set(gcf,'windowbuttonmotionfcn','');
+    set(gcf,'windowbuttonupfcn','');
+    set(gcf,'windowbuttondownfcn','');
+    set(gcf,'keypressfcn','');
+  end
+
+end
