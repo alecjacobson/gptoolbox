79824809677af63fa2e1a8d9426c5e0c0cbe3d59
Alec Jacobson (jalec <Alec Jacobson (jalec@inf.ethz.ch)>
2012-12-06 10:30:21 +0100

    readNEIGH for tetgen neighbor files

---
+++ mesh/readNEIGH.m
@@ -0,0 +1,41 @@
+function N = readNEIGH(filename)
+  % READNEIGH  Read tetrahedral neighbor information from a .neigh file (as
+  % produced by tetgen)
+  % 
+  % N = readNEIGH(filename)
+  %
+  % Inputs:
+  %   filename  path to .neigh file
+  % Outputs:
+  %   N  #simplices by #size-of-simplex neighborhood information (-1) indicates
+  %     boundary. T(i,j) indicates the neighbor to the jth face of the ith tet
+  %
+  % See also: tt, tetgen, readNODE, readELE
+  %
+
+
+  fp = fopen(filename);
+  line = fscanf(fp,' %[^\n]s');
+  [header,count] = sscanf(line,'%d %d',2);
+  if count~=2
+    fclose(fp);
+    error('Bad header');
+  end
+
+  % number of elements
+  n = header(1);
+  % size of an element
+  size_e = header(2);
+
+  parser = '%d';
+  % append to parser enough to read all entries in element + 1 for index
+  parser = [parser repmat(' %d',1,size_e+1)];
+  N = fscanf(fp,parser,[size_e+1 n])';
+  fclose(fp);
+
+  % get rid of row indices and make one indexed
+  N = N(:,2:end) + 1;
+  N(~N) = -1;
+end
+
+
