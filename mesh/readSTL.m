7e4da92a567875635778bf170b831605811112c0
Alec Jacobson <alecjacobson@gmail.com>
2014-02-26 10:34:10 +0100

    read stl files

---
+++ mesh/readSTL.m
@@ -0,0 +1,47 @@
+function [V,F] = readSTL(filename,varargin)
+  % READSTL read a triangle mesh from an .stl file.
+  %
+  % [V,F] = readSTL(filename)
+  % [V,F] = readSTL(filename,'ParameterName',ParameterValue, ...)
+  %
+  % Inputs:
+  %   filename  path to .stl file
+  %   Optional:
+  %     'JoinCorners' followed by whether to join perfectly match corners to
+  %     form a combinatorially connected mesh {false}.
+  % Outputs:
+  %   V  #V by 3 list of vertex positions
+  %   F  #F by 3 list of faces
+  % 
+
+  % default values
+  join_corners = false;
+  variable_name2 = [1,2,3];
+  % Map of parameter names to variable names
+  params_to_variables = containers.Map( {'JoinCorners'}, {'join_corners'});
+  v = 1;
+  while v <= numel(varargin)
+    param_name = varargin{v};
+    if isKey(params_to_variables,param_name)
+      assert(v+1<=numel(varargin));
+      v = v+1;
+      % Trick: use feval on anonymous function to use assignin to this workspace
+      feval(@()assignin('caller',params_to_variables(param_name),varargin{v}));
+    else
+      error('Unsupported parameter: %s',varargin{v});
+    end
+    v=v+1;
+  end 
+
+
+  [FVX,FVY,FVZ] = stlread(filename);
+  V = [FVX(:) FVY(:) FVZ(:)];
+  F = reshape(1:size(V,1),3,size(V,1)/3)';
+
+  if join_corners
+    [V,~,J] = remove_duplicate_vertices(V,0);
+    % remap faces
+    F = J(F);
+  end
+
+end
