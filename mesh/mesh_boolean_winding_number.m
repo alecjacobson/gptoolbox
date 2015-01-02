c55cc55dd4bb5b9fea6147bd075a146ae971dc14
Alec Jacobson <alecjacobson@gmail.com>
2015-01-02 10:40:11 -0500

    mesh booleans via winding number

---
+++ mesh/mesh_boolean_winding_number.m
@@ -0,0 +1,76 @@
+function [W,H] = mesh_boolean_winding_number(V,F,U,G,operation,varargin)
+  % MESH_BOOLEAN_WINDING_NUMBER Compute boolean csg operations on oriented
+  % meshes. Uses winding number to determine boolean extraction. This is
+  % theoretically prone to errors for "bad" intersections, but will handle
+  % "dirty" input meshes (e.g. open boundaries) to some degree.
+  %
+  % [W,H] = mesh_boolean(V,F,U,G,operation)
+  % 
+  % Inputs:
+  %   V  #V by 3 list of vertex positions of first mesh
+  %   F  #F by 3 list of triangle indices into V
+  %   U  #U by 3 list of vertex positions of second mesh
+  %   G  #G by 3 list of triangle indices into U
+  %   operation  followed by operation to perform as a string, one of: 'union',
+  %     'intersect', 'minus', 'xor', or 'resolve'
+  % Outputs:
+  %   W  #W by 3 list of vertex positions of boolean result mesh
+  %   H  #H by 3 list of triangle indices into W
+  %    
+  % See also: mesh_boolean
+  %
+
+  VU = [V;U];
+  FG = [F;size(V,1)+G];
+  [W,SFG,~,J,IM] = selfintersect(VU,FG);
+  [W,UIM] = remove_unreferenced(W,IM(SFG));
+  SF = IM(SFG(J<=size(F,1),:));
+  SG = IM(SFG(J>size(F,1),:));
+  SF = UIM(SF);
+  SG = UIM(SG);
+
+  th = [0.5 0.5];
+  % Map of parameter names to variable names
+  params_to_variables = containers.Map( ...
+    {'Thresholds'}, ...
+    {'th'});
+  v = 1;
+  while v <= numel(varargin)
+    param_name = varargin{v};
+    if isKey(params_to_variables,param_name)
+      assert(v+1<=numel(varargin));
+      v = v+1;
+      % Trick: use feval on anonymous function to use assignin to this workspace 
+      feval(@()assignin('caller',params_to_variables(param_name),varargin{v}));
+    else
+      error('Unsupported parameter: %s',varargin{v});
+    end
+    v=v+1;
+  end
+  A = barycenter(W,SF);
+  B = barycenter(W,SG);
+  wUGA = winding_number(U,G,A);
+  wVFB = winding_number(V,F,B);
+  switch operation
+  case 'union'
+    SF = SF(wUGA<th(1),:);
+    SG = SG(wVFB<th(2),:);
+  case 'intersect'
+    SF = SF(wUGA>=th(1),:);
+    SG = SG(wVFB>=th(2),:);
+  case 'minus'
+    SF = SF(wUGA<th(1),:);
+    SG = fliplr(SG(wVFB>=th(2),:));
+  case 'xor'
+    SF = [SF(wUGA<th(1),:); fliplr(SF(wUGA>=th(1),:))];
+    SG = [SG(wVFB<th(2),:); fliplr(SG(wVFB>=th(2),:))];
+  case 'resolve'
+    % Don't do anyting. Just snap vertices...
+  otherwise
+    error(['Unsupported operation: ' operation]);
+  end
+  % Combine meshes and map to unique vertices
+  H = [SF;SG];
+  [W,IM] = remove_unreferenced(W,H);
+  H = IM(H);
+end
