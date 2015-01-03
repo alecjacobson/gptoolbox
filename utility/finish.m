df873fc8abd39369057424a58b76777bf095ed19
Alec Jacobson <alecjacobson@gmail.com>
2015-01-02 20:03:41 -0500

    finish on desktop only

--- utility/finish.m
+++ utility/finish.m
@@ -21,12 +21,14 @@ lastworkspace = '/var/tmp/lastworkspace.mat';
 disp(['Saving workspace data to ' lastworkspace]);
 save(lastworkspace);
 
-setpref('StartupDirectory','LastWorkingDirectory',pwd) 
-try 
-    preserve_history; 
-catch EM 
-    h=msgbox(EM.message,sprintf('Error: %s',EM.identifier),'error'); 
-    uiwait(h); 
-    return 
+if usejava('desktop')
+  setpref('StartupDirectory','LastWorkingDirectory',pwd) 
+  try 
+      preserve_history; 
+  catch EM 
+      h=msgbox(EM.message,sprintf('Error: %s',EM.identifier),'error'); 
+      uiwait(h); 
+      return 
+  end
 end
 
