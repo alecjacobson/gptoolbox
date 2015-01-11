bf89689b482d24c4a676b964b737938894c67df9
Alec Jacobson <alecjacobson@gmail.com>
2015-01-11 13:17:18 -0500

    minor cleanup

--- mesh/conncomp.m
+++ mesh/conncomp.m
@@ -10,7 +10,7 @@ function [S,C] = conncomp(G)
   % Outputs:
   %   S  scalar number of connected components
   %   C  
-  [p,q,r,s,cc,rr] = dmperm(G+speye(size(G)));
+  [p,q,r] = dmperm(G+speye(size(G)));
   S = numel(r)-1;
   C = cumsum(full(sparse(1,r(1:end-1),1,1,size(G,1))));
   C(p) = C;
