6baaba8951498e07e90ff4da5f21eb02d27cabb1
Alec Jacobson <alecjacobson@gmail.com>
2015-02-11 19:55:07 -0500

    stable normals

--- mesh/normals.m
+++ mesh/normals.m
@@ -1,14 +1,48 @@
-function [ N ] = normals( V, F )
+function [ N ] = normals(V,F,varargin)
   % NORMALS Compute *unnormalized* normals per face
   %
   % N = normals(V,F)
+  % N = normals(V,F,'ParameterName',ParameterValue, ...)
   %
   % Inputs:
   %  V  #V x 3 matrix of vertex coordinates
   %  F  #F x 3  matrix of indices of triangle corners
+  %  Optional:
+  %    'Stable' followed by whether to compute normals in a way stable with
+  %    respect to vertex order: constant factor more expensive {false}
   % Output:
   %  N  #F x 3 list of face normals
   %
+
+  function D = sum3(A,B,C)
+    % SUM3 Entrywise sum of three matrices in a stable way: sorting entries by
+    % value and summing 
+    shape = size(A);
+    ABC = [A(:) B(:) C(:)];
+    [~,I] = sort(abs(ABC),2,'descend');
+    sABC = reshape( ...
+      ABC(sub2ind(size(ABC),repmat(1:size(ABC,1),size(ABC,2),1)',I)),size(ABC));
+    D = reshape(sum(sABC,2),shape);
+  end
+
+  stable = false;
+  % default values
+  % Map of parameter names to variable names
+  params_to_variables = containers.Map( ...
+    {'Stable'},{'stable'});
+  v = 1;
+  while v <= numel(varargin)
+    param_name = varargin{v};
+    if isKey(params_to_variables,param_name)
+      assert(v+1<=numel(varargin));
+      v = v+1;
+      % Trick: use feval on anonymous function to use assignin to this workspace
+      feval(@()assignin('caller',params_to_variables(param_name),varargin{v}));
+    else
+      error('Unsupported parameter: %s',varargin{v});
+    end
+    v=v+1;
+  end
   
   p1 = V(F(:,1),:);
   p2 = V(F(:,2),:);
@@ -16,7 +50,15 @@ function [ N ] = normals( V, F )
   
   % ,2 is necessary because this will produce the wrong result if there are
   % exactly 3 faces
-  N = cross(p2 - p1, p3 - p1,2);
+  N1 = cross(p2 - p1, p3 - p1,2);
+  if stable
+    N2 = cross(p3 - p2, p1 - p2,2);
+    N3 = cross(p1 - p3, p2 - p3,2);
+    N = sum3(N1,N2,N3)/3;
+  else
+    N = N1;
+  end
+
 
 end
 
