c6c5dfc3ccb5f72fad1015bb583e1aebae7f6c3b
Alec Jacobson <alecjacobson@gmail.com>
2015-02-12 19:16:14 -0500

    gif utility

--- utility/figgif.m
+++ utility/figgif.m
@@ -1,7 +1,16 @@
 function f = figgif(filename)
+  % FIGGIF Write the current figure to filename. If filename exists, append it
+  % as an animation frame with zero delay
+  %
+  % Inputs:
+  %   filename  path to gif file
+  % Outputs:
+  %   f  flag whether file already existed
+  % 
   frame = getframe(gcf);
   [SIf,cm] = rgb2ind(frame.cdata,256);
-  if ~exist(filename,'file')
+  f = exist(filename,'file');
+  if ~f
     imwrite(SIf,cm,filename,'Loop',Inf,'Delay',0);
   else
     imwrite(SIf,cm, filename,'WriteMode','append','Delay',0);
