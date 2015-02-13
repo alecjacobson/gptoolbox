db7ad2c648e297bc2a0e945df14ed4564bdfcad9
Alec Jacobson <alecjacobson@gmail.com>
2015-02-12 19:14:06 -0500

    manifold only option

--- mesh/manifold_patches.m
+++ mesh/manifold_patches.m
@@ -20,7 +20,7 @@ function [C,A,uE2F,uE] = manifold_patches(F)
   ss = size(F,2);
   assert(ss == 3);
 
-  [A,uE2F,uE] = facet_adjacency_matrix(F);
+  [A,uE2F,uE] = facet_adjacency_matrix(F,'ManifoldOnly',true);
   % Connected components are patches
   [~,C] = conncomp(A);
 
