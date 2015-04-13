41c010ae1fd64bce1aa19d5f2e07f4e968f45db2
Alec Jacobson <alecjacobson@gmail.com>
2015-04-13 17:01:22 -0400

    per_vertex_normals options

--- mesh/per_vertex_normals.m
+++ mesh/per_vertex_normals.m
@@ -1,18 +1,52 @@
-function N = per_vertex_normals(V,F)
-  % PER_VERTEX_NORMALS  Compute per-vertex normals over a mesh (V,F)
+function N = per_vertex_normals(V,F,varargin)
+  % PER_VERTEX_NORMALS  Compute per-vertex (area-weighted) normals over a mesh
+  % (V,F)
   %
   % N = per_vertex_normals(V,F)
   %
   % Inputs:
   %   V  #V by 3 list of vertex positions
   %   F  #F by 3 list of triangle indices
+  %   Optional:
+  %     'Weighting' followed by one of the following:
+  %        {'area'} weighting
+  %        'uniform' weighting
+  %        'angle' weighting
   % Outputs:
   %   N  #V by 3 list of vertex normals
   %
 
-  dblA = doublearea(V,F);
-  A= sparse(F(:),repmat(1:size(F,1),1,3),repmat(dblA,3,1),size(V,1),size(F,1));
+  weighting = 'area';
+  U = [];
+  R = [];
+  % default values
+  % Map of parameter names to variable names
+  params_to_variables = containers.Map( ...
+    {'Weighting'},{'weighting'});
+  v = 1;
+  while v <= numel(varargin)
+    param_name = varargin{v};
+    if isKey(params_to_variables,param_name)
+      assert(v+1<=numel(varargin));
+      v = v+1;
+      % Trick: use feval on anonymous function to use assignin to this workspace
+      feval(@()assignin('caller',params_to_variables(param_name),varargin{v}));
+    else
+      error('Unsupported parameter: %s',varargin{v});
+    end
+    v=v+1;
+  end
+
   FN = normalizerow(normals(V,F)+eps);
-  N = normalizerow(A*FN);
+  switch weighting
+  case 'uniform'
+    W = ones(size(F,1),3);
+  case 'angle'
+    W = internalangles(V,F);
+  case 'area'
+    W = repmat(doublearea(V,F),1,3);
+  end
+  W = sparse(F(:),repmat(1:size(F,1),1,3),W,size(V,1),size(F,1));
+  N = normalizerow(W*FN);
 
 end
