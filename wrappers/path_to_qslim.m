2f3ecfcd2b894536d02324d593dfdffa294e958b
Alec Jacobson <alecjacobson@gmail.com>
2015-04-15 09:18:09 -0400

    qslim paths

--- wrappers/path_to_qslim.m
+++ wrappers/path_to_qslim.m
@@ -1,15 +1,31 @@
-function str = path_to_qslim()
+function s = path_to_qslim()
   % PATH_TO_QSLIM
   %
-  % str = path_to_qslim()
+  % s = path_to_qslim()
   %
   % Outputs:
-  %   str  path to qslim executable
+  %   s path to qslim executable
   %
   % See also: qslim
   %
 
-  % TODO: This should be somewhere standard
-  % Replace with your location
-  str = '~/Dropbox/qslim-2.1/tools/qslim/qslim';
+  if ispc
+    warning([ ...
+      'Dear Ladislav, is there a standard place to put executables on a pc?' ...
+      'Could you put qslim there and change this accordingly?' ...
+      'Thanks, Alec']);
+    s = 'c:/prg/lib/qslim/Release/qslim.exe';
+  elseif isunix || ismac
+    % I guess this means linux
+    [status, s] = system('which qslim');
+    s = strtrim(s);
+    if isempty(s)
+      guesses = { ...
+        '/usr/local/bin/qslim', ...
+        '/opt/local/bin/qslim'};
+      s = ...
+        guesses{find(cellfun(@(guess) exist(guess,'file'),guesses),1,'first')};
+      assert(~isempty(s),'Could not find qslim');
+    end
+  end
 end
