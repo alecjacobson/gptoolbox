0827bd7ac4879f1ed7f71d5fda4e26d04d1b47b6
Alec Jacobson <alecjacobson@gmail.com>
2015-04-15 10:01:52 -0400

    updated paths

--- wrappers/path_to_medit.m
+++ wrappers/path_to_medit.m
@@ -1,7 +1,32 @@
-function MEDIT_PATH = path_to_medit()
+function s = path_to_medit()
   % PATH_TO_MEDIT Return path to medit executable
+  %
+  % s = path_to_medit()
+  %
+  % Outputs:
+  %   s path to medit executable
+  %
+  % See also: medit
+  %
 
-  MEDIT_PATH = '/usr/local/igl/libigl/external/medit/medit';
-  %MEDIT_PATH = '/opt/local/bin/medit';
+
+  if ispc
+    warning([ ...
+      'Dear Ladislav, is there a standard place to put executables on a pc?' ...
+      'Could you put medit there and change this accordingly?' ...
+      'Thanks, Alec']);
+    s = 'c:/prg/lib/medit/Release/medit.exe';
+  elseif isunix || ismac
+    % I guess this means linux
+    [status, s] = system('which medit');
+    s = strtrim(s);
+    if isempty(s)
+      guesses = { ...
+        '/usr/local/bin/medit', ...
+        '/opt/local/bin/medit'};
+      s = ...
+        guesses{find(cellfun(@(guess) exist(guess,'file'),guesses),1,'first')};
+      assert(~isempty(s),'Could not find medit');
+    end
+  end
 end
-
