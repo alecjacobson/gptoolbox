ce4415ecaa5c9b2f99c9a8dc9516b6bb9431a774
Alec Jacobson <alecjacobson@gmail.com>
2015-09-03 17:27:20 -0400

    derivation

--- mesh/vector_area_matrix.m
+++ mesh/vector_area_matrix.m
@@ -9,9 +9,23 @@ function A = vector_area_matrix(F)
   % Outputs:
   %   A  #Vx2 by #Vx2 area matrix
   %
+
   % number of vertices
   n = max(F(:));
   O = outline(F);
+  % Vector area is ∫ 1 dA = ½∫∇⋅x dA
+  % Apply divergence theorm ½∮ x⋅n dx
+  % For a discrete (planar) surface with boundary, this is 
+  %
+  % ½   ∑    ∫{ij} n⋅x dx
+  %  {ij}∈∂Ω 
+  % 
+  % Since the normal is constant along the edge use first order quadrature
+  % rules:
+  %
+  % ½  ∑     [vi - vj]⟂ [vi + vj]/2
+  %  {ij}∈∂Ω
+  %
   A = sparse( ...
     [O;O(:,[2 1])+n],[O(:,[2 1])+n;O],repmat([1 -1]/4,size(O,1)*2,1),2*n,2*n);
 end
