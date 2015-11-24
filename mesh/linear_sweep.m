5a7d5df7b4f6baa433878cb2dee5ec7897c78da3
Alec Jacobson <alecjacobson@gmail.com>
2015-11-24 18:01:25 -0500

    notes about non-robustness

--- mesh/linear_sweep.m
+++ mesh/linear_sweep.m
@@ -1,4 +1,4 @@
-function [SV,SF] = linear_sweep(V,F,sweep)
+function [SV,SF,VV,FF] = linear_sweep(V,F,sweep)
   % LINEAR_SWEEP Compute the surface of the solid sweep of a surface mesh (V,F)
   % along a vector (sweep): i.e. the Minkowski sum of (V,F) and the line
   % segment (0,0)-->(sweep) 
@@ -66,38 +66,59 @@ function [SV,SF] = linear_sweep(V,F,sweep)
     Fm = F(D<=0,:);
     O = outline(Fp);
     Fpw = [O(:,[1 2]) n+O(:,2);n+O(:,[2 1]) O(:,1)];
-    FF = [F;n+F;Fpw];
+
     VV = [V;U];
-    % Tesselated interior
-    [SVV,SFF,~,~,IM] = selfintersect(VV,FF);
-    SFF = IM(SFF);
-    [SVV,IM] = remove_unreferenced(SVV,SFF);
-    SFF = IM(SFF);
+    FFp = [fliplr(Fp);Fpw;n+Fp];
+    FFm = [Fm;Fpw;n+fliplr(Fm)];
 
-    % Can do this all using "peeling" like booleans. For now just do single
-    % outer-layer in easy case.
+    [VVp,IM] = remove_unreferenced(VV,FFp);
+    FFp = IM(FFp);
+    [VVm,IM] = remove_unreferenced(VV,FFm);
+    FFm = IM(FFm);
+    writePLY('front.ply',VVp,FFp);
+    writePLY('back.ply',VVm,FFm);
+    statistics(VVp,FFp)
+    statistics(VVm,FFm)
+    [SV,SF] = mesh_boolean(VVp,FFp,VVm,FFm,'union');
 
-    % check if bounding boxes overlap
-    [BV,BF] = bounding_box(V);
-    BU = bsxfun(@plus,BV,sweep);
-    if all(round(winding_number(BV,BF,BU))==0)
-      SF = outer_hull(SVV,SFF);
-      [SV,IM] = remove_unreferenced(SVV,SF);
-      SF = IM(SF);
-    else
-      %statistics(SVV,SFF)
-      [TV,TT,TF] = tetgen(SVV,SFF,'Flags','-YT1e-16');
-      % (self-intersecting) boundary Sweep
-      WF = [Fm;n+Fp;Fpw];
-      WV = [V;U];
-      BC = barycenter(TV,TT);
-      % Classify interior elements with winding number
-      w = winding_number(WV,WF,BC);
-      % Extract boundary of w~=0 part
-      SF = boundary_faces(TT(round(w)~=0,:));
-      [SV,IM] = remove_unreferenced(TV,SF);
-      SF = IM(SF);
-    end
+    %% (self-intersecting) boundary Sweep
+    %FF = [Fm;n+Fp;Fpw];
+    %VV = [V;U];
+
+    %%% Self-union: this doesn't work... Non-manifold edges in Fpw? Yes, this is
+    %%% possible (folded Y case), but why is it a problem?
+    %%warning('experimental. not working.');
+    %%[SV,SF] = mesh_boolean(VV,FF,[],[],'union');
+    %%return;
+
+    %% Tesselated interior
+    %[SVV,SFF,~,~,IM] = selfintersect(VV,FF);
+    %SFF = IM(SFF);
+    %[SVV,IM] = remove_unreferenced(SVV,SFF);
+    %SFF = IM(SFF);
+
+    %% Can do this all using "peeling" like booleans. For now just do single
+    %% outer-layer in easy case.
+
+    %% check if bounding boxes overlap
+    %[BV,BF] = bounding_box(V);
+    %BU = bsxfun(@plus,BV,sweep);
+    %if false && all(round(winding_number(BV,BF,BU))==0)
+    %  SF = outer_hull(SVV,SFF);
+    %  [SV,IM] = remove_unreferenced(SVV,SF);
+    %  SF = IM(SF);
+    %else
+    %  %statistics(SVV,SFF)
+    %  [TV,TT,TF] = tetgen(SVV,SFF,'Flags','-YT1e-16');
+    %  BC = barycenter(TV,TT);
+    %  % Classify interior elements with winding number
+    %  w = winding_number(VV,FF,BC);
+    %  numel(w(abs(w-round(w))>1e-7))
+    %  % Extract boundary of w~=0 part
+    %  SF = boundary_faces(TT(round(w)~=0,:));
+    %  [SV,IM] = remove_unreferenced(TV,SF);
+    %  SF = IM(SF);
+    %end
   end
 
 end
