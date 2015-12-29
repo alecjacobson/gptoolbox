9f64c93b7ca889d90b33c4d4cc4560232958e8aa
Alec Jacobson <alecjacobson@gmail.com>
2015-12-29 11:19:42 -0600

    read face force no boundary

--- mesh/readFACE.m
+++ mesh/readFACE.m
@@ -1,33 +1,81 @@
-function [F,B] = readFACE(filename)
+function [F,B] = readFACE(filename,varargin)
   % READFACE
   %
   %[F,B] = readFACE(filename)
   %
   % Read triangular faces from a .face file
+  %
   % Input:
   %  filename  name of .face file
   % Output:
   %  F  list of triangle indices
   %  B  list of boundary markers
+  %
+
+  force_no_boundary = false;
+  % default values
+  % Map of parameter names to variable names
+  params_to_variables = containers.Map( ...
+    {'ForceNoBoundary'}, ...
+    {'force_no_boundary'});
+  v = 1;
+  while v <= numel(varargin)
+    param_name = varargin{v};
+    if isKey(params_to_variables,param_name)
+      assert(v+1<=numel(varargin));
+      v = v+1;
+      % Trick: use feval on anonymous function to use assignin to this workspace
+      feval(@()assignin('caller',params_to_variables(param_name),varargin{v}));
+    else
+      error('Unsupported parameter: %s',varargin{v});
+    end
+    v=v+1;
+  end
+
   fp = fopen(filename,'r');
-  header = fscanf(fp,'%d %d',2);
+  header = fscanf(fp,'%d %d\n',2);
   sizeF = header(1);
   boundary_markers = header(2);
-  parser = '%d %d %d %d';
-  num_items = 4;
-  if(boundary_markers ~= 0)
-    parser = [parser ' %d'];
-    num_items = 5;
+  if force_no_boundary
+    F = zeros(10000,3);
+    f = 0;
+    while true
+      line = fgets(fp);
+      if line == -1
+        break;
+      end
+      if isempty(line)
+        continue;
+      end
+      if line(1) == '#'
+        continue
+      end
+      face = sscanf(line,'%d %d %d %d',4);
+      f = f+1;
+      % Resize
+      if f>size(F,1)
+        F = [F;zeros(size(F,1),size(F,2))];
+      end
+      F(f,:) = face(2:4);
+    end
+    F = F(1:f,:);
+  else
+    parser = '%d %d %d %d';
+    num_items = 4;
+    if(boundary_markers ~= 0)
+      parser = [parser ' %d'];
+      num_items = 5;
+    end
+  
+    F = fscanf(fp,parser,num_items*sizeF);
+    fclose(fp);
+  
+    F = reshape(F,num_items,sizeF)';
+    B = [];
+    if boundary_markers
+      B = F(:,5);
+    end
+    % get rid of indices and boundary markers and make one indexed
+    F = F(:,2:4);
   end
-
-  F = fscanf(fp,parser,num_items*sizeF);
-  fclose(fp);
-
-  F = reshape(F,num_items,sizeF)';
-  B = [];
-  if boundary_markers
-    B = F(:,5);
-  end
-  % get rid of indices and boundary markers and make one indexed
-  F = F(:,2:4);
 end
