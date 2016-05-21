90300de1d4df963dc0528a9d363f30ec2547c4cd
Alec Jacobson <alecjacobson@gmail.com>
2016-05-21 14:51:27 -0400

    support for 2-simplices

--- mesh/barycentric_coordinates.m
+++ mesh/barycentric_coordinates.m
@@ -48,6 +48,16 @@ function B = barycentric_coordinates(P,varargin)
       warning('Possibly negative coordinates. Not supported in dim~=2');
     end
     B = bsxfun(@rdivide,[A1 A2 A3],A);
+  case 2
+    V1 = varargin{1};
+    V2 = varargin{2};
+    A1 = edge_lengths([P;V2],[1:n;n+[1:n]]');
+    A2 = edge_lengths([V1;P],[1:n;n+[1:n]]');
+    A = edge_lengths([V1;V2],[1:n;n+[1:n]]');
+    if size(P,2)>1 && max(abs(sum([A1 A3],2)-A))>1e-14
+      warning('Possibly negative coordinates. Not supported in dim~=1');
+    end
+    B = bsxfun(@rdivide,[A1 A2],A);
   otherwise
     error(sprintf('%d-simplices not supported',numel(varargin)));
   end
