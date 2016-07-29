57a04061798a78e84f8bf0b5e71b809cab2842f6
Alec Jacobson <alecjacobson@gmail.com>
2016-07-29 10:20:38 -0700

    J output

--- mex/decimate_libigl.cpp
+++ mex/decimate_libigl.cpp
@@ -28,6 +28,7 @@ void mexFunction(
   using namespace Eigen;
   MatrixXd V,W;
   MatrixXi F,G;
+  VectorXi J;
 
   igl::matlab::MexStream mout;        
   std::streambuf *outbuf = std::cout.rdbuf(&mout);
@@ -43,10 +44,12 @@ void mexFunction(
   double ratio = * mxGetPr(prhs[2]);
   mexErrMsgTxt((ratio>0 && ratio<1) || (ratio>0 && ratio<F.rows()) ,"Ratio should be in (0,1) or [1,#F)");
   const size_t max_m = ratio<1 ? ratio*F.rows() : ratio;
-  decimate(V,F,max_m,W,G);
+  decimate(V,F,max_m,W,G,J);
 
   switch(nlhs)
   {
+    case 3:
+      prepare_lhs_index(J,plhs+2);
     case 2:
       prepare_lhs_index(G,plhs+1);
     case 1:
