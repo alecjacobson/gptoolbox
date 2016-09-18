9d08583953483f70b8473521e05a648a281b100c
Alec Jacobson <alecjacobson@gmail.com>
2016-09-18 16:38:56 -0400

    output values

--- mesh/slice_isolines.m
+++ mesh/slice_isolines.m
@@ -1,4 +1,4 @@
-function [U,G,J,BC,SU] = slice_isolines(V,F,SV,val,varargin)
+function [U,G,J,BC,SU,L] = slice_isolines(V,F,SV,val,varargin)
   % SLICE_ISOLINES Slice through a triangle mesh (V,F) at isolines val of a given
   % per-vertex scalar function.
   %
@@ -21,6 +21,7 @@ function [U,G,J,BC,SU] = slice_isolines(V,F,SV,val,varargin)
   %   BC  #U by #V list of barycentric coordinates (or more generally: linear
   %     interpolation coordinates) so that U = BC*V
   %   SU  #U list of interpolated scalar values at U
+  %   L  #G list of indices into val of next value above this triangle
   %
 
   manifold = false;
@@ -45,8 +46,8 @@ function [U,G,J,BC,SU] = slice_isolines(V,F,SV,val,varargin)
   end
 
   % helper assuming val is a scalar
-  function [U,G,J,BC,SU] = single_val(V,F,SV,val)
-    function [U,G,BC] = one_below(V,F,SF,val)
+  function [U,G,J,BC,SU,L] = single_val(V,F,SV,val)
+    function [U,G,BC,L] = one_below(V,F,SF,val)
       [sSF,sJ] = sort(SF,2);
       sF = F(sub2ind(size(F),repmat(1:size(F,1),size(F,2),1)',sJ));
       lambda = (sSF(:,2:3)-val)./bsxfun(@minus,sSF(:,2:3),sSF(:,1));
@@ -62,6 +63,7 @@ function [U,G,J,BC,SU] = slice_isolines(V,F,SV,val,varargin)
         fliplr([sF(:,2) size(V,1)+[1:size(F,1);size(F,1)+(1:size(F,1))]']); ...
         sF(:,[2 3]) size(V,1)+[size(F,1)+(1:size(F,1))]'; ...
         ];
+      L = [ones(size(sF,1),1);2*ones(2*size(sF,1),1)];
       flip = repmat( ...
         (sJ(:,1)==1 & sJ(:,2)==3) | ...
         (sJ(:,1)==2 & sJ(:,2)==1) | ...
@@ -72,23 +74,27 @@ function [U,G,J,BC,SU] = slice_isolines(V,F,SV,val,varargin)
     I12 = sum(SF<val,2) == 1;
     % U is the running set of vertices
     U = V;
-    [U,G12,BC12] = one_below(U,F(I12,:),SF(I12,:),val);
+    [U,G12,BC12,L12] = one_below(U,F(I12,:),SF(I12,:),val);
     I21 = sum(SF>=val,2) == 1;
-    [U,G21,BC21] = one_below(U,F(I21,:),2*val-SF(I21,:),val);
+    [U,G21,BC21,L21] = one_below(U,F(I21,:),2*val-SF(I21,:),val);
     BC = [speye(size(V,1));BC12;BC21(:,1:size(V,1))];
     untouched = find(~I12&~I21);
     G = [F(untouched,:);G12;G21];
     J = [untouched;repmat(find(I12),3,1);repmat(find(I21),3,1)];
     SU = BC*SV;
+    L = [(sum(SF(untouched,:)>=val,2)==3)+1;L12;3-L21];
   end
 
   U = V;
   G = F;
   SU = SV;
   J = 1:size(F,1);
+  L = ones(size(F,1),1);
   for v = 1:numel(val)
     prev_J = J;
-    [U,G,J,BC,SU] = single_val(U,G,SU,val(v));
+    prev_L = L;
+    [U,G,J,BC,SU,L] = single_val(U,G,SU,val(v));
+    L = prev_L(J)+(L-1);
     J = prev_J(J);
   end
 
