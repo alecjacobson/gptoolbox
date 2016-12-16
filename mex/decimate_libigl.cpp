7e00ec5b9d8de19072d30782bcc842e0c6bc6688
Alec Jacobson <alecjacobson@gmail.com>
2016-12-16 16:35:39 -0500

    decimate with qslim, point mesh sqrD with cgal

--- mex/decimate_libigl.cpp
+++ mex/decimate_libigl.cpp
@@ -1,4 +1,5 @@
 #include <igl/decimate.h>
+#include <igl/qslim.h>
 #include <Eigen/Core>
 #include <iostream>
 #include <set>
@@ -8,7 +9,7 @@
 #  include <igl/C_STR.h>
 #  include <igl/matlab/mexErrMsgTxt.h>
 #  undef assert
-#  define assert( isOK ) ( (isOK) ? (void)0 : (void) mexErrMsgTxt(C_STR(__FILE__<<":"<<__LINE__<<": failed assertion `"<<#isOK<<"'"<<std::endl) ) )
+#  define assert( isOK ) ( (isOK) ? (void)0 : (void) ::mexErrMsgTxt(C_STR(__FILE__<<":"<<__LINE__<<": failed assertion `"<<#isOK<<"'"<<std::endl) ) )
 #endif
 
 #include <igl/matlab/MexStream.h>
@@ -28,7 +29,7 @@ void mexFunction(
   using namespace Eigen;
   MatrixXd V,W;
   MatrixXi F,G;
-  VectorXi J;
+  VectorXi J,I;
 
   igl::matlab::MexStream mout;        
   std::streambuf *outbuf = std::cout.rdbuf(&mout);
@@ -42,12 +43,64 @@ void mexFunction(
     mxIsDouble(prhs[2]) && mxGetM(prhs[2])==1 && mxGetN(prhs[2])==1,
     "fraction to decimate should be scalar");
   double ratio = * mxGetPr(prhs[2]);
-  mexErrMsgTxt((ratio>0 && ratio<1) || (ratio>0 && ratio<F.rows()) ,"Ratio should be in (0,1) or [1,#F)");
+  mexErrMsgTxt((ratio>0 && ratio<1) || (ratio>0 && ratio<F.rows()) ,
+    "Ratio should be in (0,1) or [1,#F)");
   const size_t max_m = ratio<1 ? ratio*F.rows() : ratio;
-  decimate(V,F,max_m,W,G,J);
+
+
+  enum DecimateMethod
+  {
+    DECIMATE_METHOD_NAIVE = 0,
+    DECIMATE_METHOD_QSLIM = 1,
+    NUM_DECIMATE_METHODS = 2
+  } method = DECIMATE_METHOD_NAIVE;
+  {
+    int i = 3;
+    while(i<nrhs)
+    {
+      mexErrMsgTxt(mxIsChar(prhs[i]),"Parameter names should be strings");
+      // Cast to char
+      const char * name = mxArrayToString(prhs[i]);
+      if(strcmp("Method",name) == 0)
+      {
+        validate_arg_char(i,nrhs,prhs,name);
+        const char * type_name = mxArrayToString(prhs[++i]);
+        if(strcmp("naive",type_name)==0)
+        {
+          method = DECIMATE_METHOD_NAIVE;
+        }else if(strcmp("qslim",type_name)==0)
+        {
+          method = DECIMATE_METHOD_QSLIM;
+        }else
+        {
+          mexErrMsgTxt(false,C_STR("Unknown method: "<<method));
+        }
+      }else
+      {
+        mexErrMsgTxt(false,C_STR("Unknown parameter: "<<name));
+      }
+      i++;
+    }
+  }
+
+
+  switch(method)
+  {
+    case DECIMATE_METHOD_NAIVE:
+      decimate(V,F,max_m,W,G,J,I);
+      break;
+    case DECIMATE_METHOD_QSLIM:
+      qslim(V,F,max_m,W,G,J,I);
+      break;
+    default:
+      mexErrMsgTxt(false,"Unkown method.");
+      break;
+  }
 
   switch(nlhs)
   {
+    case 4:
+      prepare_lhs_index(I,plhs+3);
     case 3:
       prepare_lhs_index(J,plhs+2);
     case 2:
