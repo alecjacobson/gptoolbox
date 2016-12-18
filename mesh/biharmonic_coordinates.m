a6b0238591eb9152cde4967939a20f32b103cd10
Alec Jacobson <alecjacobson@gmail.com>
2016-12-18 16:10:56 -0500

    ears warning

--- mesh/biharmonic_coordinates.m
+++ mesh/biharmonic_coordinates.m
@@ -6,6 +6,7 @@ function [W,A,K,M,L,N] = biharmonic_coordinates(V,Ele,b)
   % [Weber et al. 12].
   %
   % W = biharmonic_coordinates(V,Ele,b)
+  % [W,A,K,M,L,N] = biharmonic_coordinates(V,Ele,b)
   %
   % Inputs:
   %   V  #V by dim list of vertex positions
@@ -14,7 +15,16 @@ function [W,A,K,M,L,N] = biharmonic_coordinates(V,Ele,b)
   % Outputs:
   %   W  #V by #b list of coordinates
   %   A  #V by #V quadratic coefficients matrix
+  %   K  #V by #V "Laplacian" so that K = L + N, where L is the "usual"
+  %     cotangent Laplacian and N computes normal derivatives at boundary
+  %     vertices.
   %
+
+  if size(Ele,2) == 3
+    assert(isempty(find_ears(Ele)), ...
+      'Mesh should not have ears, preprocess Ele with flip_ears');
+  end
+
   L = cotmatrix(V,Ele);
   [DD,TE] = normal_derivative(V,Ele);
   AT = sparse( ...
@@ -27,7 +37,7 @@ function [W,A,K,M,L,N] = biharmonic_coordinates(V,Ele,b)
   K = L + N;
   M = massmatrix(V,Ele);
   A = K' * (M\K);
-  if isempty(b)
+  if nargin<3 || isempty(b)
     W = [];
   else
     W = min_quad_with_fixed(A,[],b,eye(numel(b)));
