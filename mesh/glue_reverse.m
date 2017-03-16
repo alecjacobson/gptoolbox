f651e1e769f0c3fc26d390d98caf3cedff07aac3
Alec Jacobson <alecjacobson@gmail.com>
2017-03-16 12:05:08 -0400

    optional parameters

--- mesh/glue_reverse.m
+++ mesh/glue_reverse.m
@@ -1,4 +1,4 @@
-function [VV,FF,RIM] = glue_reverse(V,F)
+function [VV,FF,RIM] = glue_reverse(V,F,varargin)
   % GLUE_REVERSE Glue a mesh to itself at its boundary reversing the
   % z-coordinates of one of the copies. Useful for creating closed meshes from
   % inflated disks.
@@ -8,11 +8,34 @@ function [VV,FF,RIM] = glue_reverse(V,F)
   % Inputs:
   %   V  #V by 3  list of vertex positions
   %   F  #F by 3  list of face indices
+  %   Optional:
+  %     'Boundary'  followed #E by 2 by subset of the boundary edges to glue
+  %       along. {all boundary edges}
   % Outputs:
   %  VV #VV by 3 list of new vertex positions
   %  FF #FF by 3 list of new face indices
-  %  RIM #V by 1 list of reindexing indices such that V(RIM,:) = VV
+  %  RIM #VV by 1 list of reindexing indices such that VV = V(RIM,:)
   %
+
+  % default values
+  out = [];
+  % Map of parameter names to variable names
+  params_to_variables = containers.Map( ...
+    {'Boundary'}, ...
+    {'out'});
+  v = 1;
+  while v <= numel(varargin)
+    param_name = varargin{v};
+    if isKey(params_to_variables,param_name)
+      assert(v+1<=numel(varargin));
+      v = v+1;
+      % Trick: use feval on anonymous function to use assignin to this workspace
+      feval(@()assignin('caller',params_to_variables(param_name),varargin{v}));
+    else
+      error('Unsupported parameter: %s',varargin{v});
+    end
+    v=v+1;
+  end
 
   % number of domain vertices
   n = size(V,1);
@@ -20,7 +43,9 @@ function [VV,FF,RIM] = glue_reverse(V,F)
   % concatenate vertex lists with Z-coord of copy flipped
   VV = [V; V(:,1:2) -1*V(:,3:dim)];
   % get list of unique boundary vertex indices
-  out = unique(reshape(outline(F),[],1));
+  if isempty(out)
+    out = unique(reshape(outline(F),[],1));
+  end
   VV(out,3:dim) = 0;
   % get list of new faces as if we'd just concatenate vertex lists, normals of new
   % faces are flipped
