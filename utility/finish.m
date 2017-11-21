b5f37214c5bf941084b1424880bffd376ddf68df
Alec Jacobson <alecjacobson@gmail.com>
2017-11-21 18:15:10 -0500

    load prev workspace

--- utility/finish.m
+++ utility/finish.m
@@ -26,12 +26,18 @@ else
 end
 
 if usejava('desktop')
-  setpref('StartupDirectory','LastWorkingDirectory',pwd) 
-  try 
-      preserve_history; 
-  catch EM 
-      h=msgbox(EM.message,sprintf('Error: %s',EM.identifier),'error'); 
-      uiwait(h); 
-      return 
+  setpref('my','LastWorkingDirectory',pwd)
+  
+  if ~exist('preserve_history_failed_','var')
+      preserve_history_failed_ = false;
+  end
+  try
+      if ~preserve_history_failed_
+          preserve_history;
+      end
+  catch
+      preserve_history_failed_ = true;
+      disp Exiting Matlab was halted so you can investigate.
+      disp Type "preserve_history" after you fix it before exiting this Matlab session.
   end
 end
