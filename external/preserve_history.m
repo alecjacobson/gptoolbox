c1e3c78a54025b1200642bed7d8a9b518c927c64
Alec Jacobson <alecjacobson@gmail.com>
2017-11-21 18:22:55 -0500

    new version

--- external/preserve_history.m
+++ external/preserve_history.m
@@ -1,66 +1,70 @@
-% JS - 11/01/2005
-% Maintains command history indefinitely in file "history_saved.m"
+function preserve_history ()
+% Maintains command history indefinitely in a separate history_saved.m file
+% since Matlab is truncating history.m to a fixed size. The code is fast even
+% when history_saved.m gets very big.
 %
-% This is handy because Matlab limits the history file to 20k in size, so
-% throws away old commands (which are invariably the ones you want to recall).
-% To automate history preservation call this script from "startup.m" or
-% "finish.m"
+% Also works with the new format introduced in R2014a which is XML. Its
+% contents are parsed as plain commands and merged into history_saved.m
+%
+% Bogdan Roman, 2014, abr28@cam.ac.uk, University of Cambridge
 
-% find last date matlab was opened in 'preserved' history file
-[fid, err] = fopen(fullfile(prefdir,'history_saved.m'), 'r'); 
+savpath = fullfile(prefdir,'history_saved.m');
 
-i=0;
-while ~feof(fid)
-    i=i+1;
-    line = fgetl(fid);
-    if regexp(line,'%-- ') == 1
-        date_line = line;
-        date_line_pos = ftell(fid);
-    end
+% If History.xml exists (R2014a or later) read that, else revert to history.m
+hpath = fullfile(prefdir,'History.xml');
+if exist(hpath,'file')
+    % Read the XML file into a string to look as the older history.m (i.e.
+    % remove all XML tags)
+    mathist = fileread(hpath);
+    mathist = regexprep(mathist, '(<[^>]+>\s*)+', '\n', 'lineanchors');
+    % translate html entities and remove leading newline
+    mathist = strrep(mathist(3:end), '&gt;', '>');
+    mathist = strrep(mathist, '&lt;', '<');
+else
+    mathist = fileread(fullfile(prefdir,'history.m'));
 end
 
-%date_line;
-fclose(fid);
+% replace \r and \r\n with \n (safe to copy between OSes)
+mathist = regexprep(mathist, '\r(\n)?', '\n');
 
-% match last date from 'preserved' file to a line in current 'history.m' file, then append new lines in 'history.m' to 'preserved' file
-[fid, err] = fopen(fullfile(prefdir,'history.m'), 'r'); 
+fprintf('Merging current command history into %s ...\n', savpath);
 
-found = 0;
-i=0;
-while ~feof(fid)
-    i=i+1;
-    line = fgetl(fid);
-    whole_file{i,:} = line;
-    if regexp(line,date_line) == 1
-        alpha = 1;
-        while ~feof(fid)
-            found = 1;
-            line = fgetl(fid);
-            append{alpha,:} = line;
-            alpha = alpha + 1;
-        end
-    end
-end
-
-fclose(fid);
-
-if found == 1                   % add commands in 'append' to end of saved
-    [fid, err] = fopen(fullfile(prefdir,'history_saved.m'), 'r'); 
-
-    fseek(fid,date_line_pos,'bof');             % move to last recognised date in saved file
-    for i=1:length(append)
-        fprintf(fid, '%s\r\n', append{i});
-    end
-
+% if empty saved file, dump it all
+if ~exist(savpath,'file')
+    fid = fopen(savpath, 'w');
+    fwrite(fid, mathist);
     fclose(fid);
-
-    % elseif found == 0               % add ALL commands from current to end of saved
-    %     fseek(fid,0,'eof');             % move to end of file
-    %     for i=1:length(whole_file)
-    %         fprintf(fid, '%s\r\n', whole_file{i});
-    %     end
-    %
+    return;
 end
 
+% Read saved file fully and use regexp(). Faster than parsing line by line.
+savhist = fileread(savpath);
+%savhist = regexprep(savhist, '\r(\n)?', '\n');
 
-disp('History Preserved');
+% Look for last saved date in the preserved file. Must be a valid date and
+% not 'Unknown date', which may match in both files and overwrite the entire
+% saved history. Note the \d to that effect.
+[savstart, savend] = regexp(savhist,'^%-- \d.*?$','lineanchors');
+if isempty(savstart)
+    % If none found, then append current history entirely
+    fid = fopen(savpath,'a');
+    fwrite(fid, mathist);
+    fclose(fid);
+else
+    % Extract last save date as string and search for it in current history
+    savlastdate = savhist(savstart(end):savend(end)-1);
+    matstart = regexp(mathist, ['^' savlastdate], 'lineanchors');
+    % If not found, append the entire current history
+    if isempty(matstart)
+        fid = fopen(savpath,'a');
+        fwrite(fid, mathist);
+        fclose(fid);
+    % Otherwise merge the chunks
+    else
+        fid = fopen(savpath, 'r+');
+        fseek(fid, savstart(end)-1, 'bof');
+        fwrite(fid, mathist(matstart(1):end));
+        fclose(fid);
+    end
+end
+disp('Done');
