95496d7ca628831d81c3e8786ec0a12be7bb988e
Alec Jacobson <alecjacobson@gmail.com>
2018-01-05 13:36:28 -0500

    fixed orientation problem

--- mesh/polygonize.m
+++ mesh/polygonize.m
@@ -1,4 +1,4 @@
-function [CV,CE] = polygonize(V,F,fun)
+function [CV,CE,I] = polygonize(V,F,fun)
   % Polygonize (contour) an implicit function in the spirit of "An Implicit
   % Surface Polygonizer" [Bloomenthal 1994]
   %
@@ -11,11 +11,19 @@ function [CV,CE] = polygonize(V,F,fun)
   % Outputs:
   %   CV  #CV by dim list of contour mesh vertices 
   %   CE  #CE by dim list of facet indices into CV
+  %   I  #CE list of indices into F
   %
+
+  flipped_order = flipped_tet_orders();
+
   max_iters = 10;
   D = fun(V);
-  interval = @(DE) any(DE>0.0,2) & any(DE<=0.0,2);
-  FF = F(interval(D(F)),:);
+  interval = @(DF) any(DF>0.0,2) & any(DF<=0.0,2);
+  crosses = @(DE) ...
+    (DE(:,1)>0 & DE(:,2)<= 0)*-1 + ...
+    (DE(:,2)>0 & DE(:,1)<= 0)*1;
+  crossing_F = find(interval(D(F)));
+  FF = F(crossing_F,:);
   % simplex size
   ss = size(F,2);
   switch ss
@@ -31,8 +39,10 @@ function [CV,CE] = polygonize(V,F,fun)
        FF(:,3) FF(:,4) ...
        ];
   end
-  [E,~,EMAP] = unique(sort(allE,2),'rows');
-  crossing = interval(D(E));
+  sE = sort(allE,2);
+  [E,~,EMAP] = unique(sE,'rows');
+  cross_dir = crosses(D(E));
+  crossing = cross_dir ~= 0;
   J = (1:size(E,1))';
   EE = E(crossing,:);
   J(crossing) = 1:size(EE,1);
@@ -46,13 +56,58 @@ function [CV,CE] = polygonize(V,F,fun)
     % CE(f,i) is the index of the unique edge that does cross
     CE = reshape(crossing(EMAP),[],6).*reshape(J(EMAP),[],6);
     % If 3 edges cross then we can surface with a single triangle
-    CT = sort(CE(sum(CE>0,2)==3,:),2);
+    crossE = reshape(cross_dir(EMAP),[],6);
+    IT = find(sum(CE>0,2)==3);
+    crossT = crossE(IT,:);
+    [CT,ST] = sort(CE(IT,:),2);
     CT = CT(:,end-2:end);
-    CQ = sort(CE(sum(CE>0,2)==4,:),2);
+    flippedT = [
+      -1 -1 -1 0 0 0 4 5 6 1 3 2
+      -1 -1 -1 0 0 0 4 5 6 2 1 3
+      -1 0 0 1 1 0 2 3 6 1 5 4
+      0 -1 0 -1 0 -1 1 3 5 2 4 6
+      0 -1 0 -1 0 1 1 3 5 2 4 6
+      0 -1 0 1 0 1 1 3 5 2 4 6
+      0 0 -1 0 -1 -1 1 2 4 3 6 5
+      0 0 -1 0 1 -1 1 2 4 3 6 5
+      0 0 1 0 1 -1 1 2 4 3 5 6
+      0 0 1 0 1 1 1 2 4 3 5 6
+      0 1 0 -1 0 -1 1 3 5 2 6 4
+      1 0 0 -1 -1 0 2 3 6 1 4 5
+      1 0 0 1 -1 0 2 3 6 1 4 5
+      1 0 0 1 1 0 2 3 6 1 4 5
+      1 1 1 0 0 0 4 5 6 1 2 3
+      1 1 1 0 0 0 4 5 6 2 3 1
+    ];
+    fT = ismember([crossT ST],flippedT,'rows');
+    CT(fT,:) = fliplr(CT(fT,:));
+
+    IQ = find(sum(CE>0,2)==4);
+    crossQ = crossE(IQ,:);
+    [CQ,SQ] = sort(CE(IQ,:),2);
     CQ = CQ(:,end-3:end);
-    CE = [CT;CQ(:,[1 4 3]);CQ(:,[1 2 4])];
+    flippedQ = [
+      -1 -1 0 0 -1 1 3 4 2 1 6 5
+      -1 -1 0 0 1 1 3 4 2 1 6 5
+      -1 0 -1 -1 0 -1 2 5 1 3 4 6
+      -1 0 -1 1 0 -1 2 5 1 3 4 6
+      -1 0 -1 1 0 1 2 5 1 3 4 6
+      0 -1 -1 -1 -1 0 1 6 3 2 5 4
+      0 1 1 -1 -1 0 1 6 2 3 4 5
+      0 1 1 -1 1 0 1 6 2 3 4 5
+      0 1 1 1 1 0 1 6 2 3 4 5
+      1 0 1 1 0 1 2 5 3 1 6 4
+      1 1 0 0 -1 -1 3 4 1 2 5 6
+      1 1 0 0 -1 1 3 4 1 2 5 6
+    ]; 
+    fQ = ismember([crossQ SQ],flippedQ,'rows');
+    %% huh. Need to flip _after_ creating triangles.
+    %CQT([fQ;fQ],:) = fliplr(CQT([fQ;fQ],:));
+    % or switch the middle two...
+    CQ(fQ,:) = CQ(fQ,[1 3 2 4]);
+    I = [IT;IQ;IQ];
+    I = crossing_F(I);
   end
-  assert(size(CE,2) == ss-1);
   % Upper and lower bound on barycenteric coordinate locating =0.5
   EEl= zeros(size(EE,1),1);
   EElV = V(EE(:,1),:);
@@ -74,4 +129,41 @@ function [CV,CE] = polygonize(V,F,fun)
        Dl(~front,:) =  Dm(~front,:);
     end
   end
+
+  % Flip non-deluanay edges in quads... at least.
+  CQT = [CQ(:,[3 1 4]);CQ(:,[2 4 1])];
+  A = reshape(internalangles(CV,CQT),[],6);
+  % Quads with Non-delaunay diagonals.
+  nd = (A(:,1)+A(:,4))>pi;
+  CQ(nd,:) = CQ(nd,[3 4 1 2]);
+  CQ1 = CQ(:,[3 1 4]);
+  CQ2 = CQ(:,[2 4 1]);
+  CQ1(nd,:) = fliplr(CQ1(nd,:));
+  CQ2(nd,:) = fliplr(CQ2(nd,:));
+  CQT = [CQ1;CQ2];
+  %% Double check: sum should be zero
+  % A = reshape(internalangles(CV,CQT),[],6);
+  % % Non-delaunay
+  % nd = (A(:,1)+A(:,4))>pi;
+  % sum(nd)
+  
+
+  CE = [CT;CQT];
+  assert(size(CE,2) == ss-1);
+
+  %% How I determined what should be flipped:
+  % S = sum(normals(CV,CT).*barycenter(CV,CT),2);
+  % A = unique([crossT(S<0,:) ST(S<0,:)],'rows');
+  % B = unique([crossT(S>0,:) ST(S>0,:)],'rows');
+  % intersect(A,B,'rows');
+  % fprintf('    flippedT = [\n');
+  % fprintf('      %d %d %d %d %d %d %d %d %d %d %d %d\n',A');
+  % fprintf('    ];\n');
+  %S = sum(normals(CV,CQ(:,[1 4 3])).*barycenter(CV,CQ(:,[1 4 3])),2);
+  %A = unique([crossQ(S<0,:) SQ(S<0,:)],'rows');
+  %B = unique([crossQ(S>0,:) SQ(S>0,:)],'rows');
+  %intersect(A,B,'rows')
+  %fprintf('    flippedQ = [\n');
+  %fprintf('      %d %d %d %d %d %d %d %d %d %d %d %d\n',A');
+  %fprintf('    ];\n');
 end
