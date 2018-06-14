b6f12413e08ec5eb835a814d2dc7f88857556f41
Alec Jacobson <alecjacobson@gmail.com>
2018-06-14 19:25:37 -0400

    pink and teal colors

---
+++ utility/pink.m
@@ -0,0 +1,8 @@
+function C = pink()
+  % PINK Returns a nice pink color
+  %
+  % Outputs:
+  %   [254 194 194]/255;
+  C = [254 194 194]/255;;
+end
+
---
+++ utility/teal.m
@@ -0,0 +1,8 @@
+function C = teal()
+  % TEAL Returns a nice teal color
+  %
+  % Outputs:
+  %   [144 216 196]/255
+  C = [144 216 196]/255;
+end
+
