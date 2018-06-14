87616c77f142ef30fe9262aba27cd805cd2a820d
Alec Jacobson <alecjacobson@gmail.com>
2018-06-14 19:24:36 -0400

    use embree

--- mex/ray_mesh_intersect.cpp
+++ mex/ray_mesh_intersect.cpp
@@ -1,126 +1,95 @@
-#include "ray_mesh_intersect.h"
+#include <igl/embree/EmbreeIntersector.h>
+#include <igl/Hit.h>
+#include <igl/matlab/mexErrMsgTxt.h>
+#include <igl/matlab/parse_rhs.h>
+#include <igl/matlab/prepare_lhs.h>
+#undef assert
+#define assert( isOK ) ( (isOK) ? (void)0 : (void) mexErrMsgTxt(C_STR(__FILE__<<":"<<__LINE__<<": failed assertion `"<<#isOK<<"'"<<std::endl) ) )
+#include <igl/matlab/MexStream.h>
+#include <Eigen/Core>
+#include <limits>
+#include <mex.h>
 
-/*
- * RAY_MESH_INTERSECT  Ray/mesh intersection using the algorithm proposed by
- * Möller and Trumbore (1997).
- *
- * [flag, t, lambda_1, lambda_2, lambda_3] = ray_mesh_intersect(o, d, V, F);
- *
- * Input:
- *    o  3D vector ray origin.
- *    d  3D vector ray direction.
- *    V  #V by 3 list of vertex positions
- *    F  #F by 3 list of triangle indices
- * Output:
- *    flag  #F list of bools: (false) Reject, (true) Intersect.
- *    t  #F list of distances from the ray origin.
- *    lambda: #F by 3 list of barycentric coordinate of hits on each triangle
- * Modified from code by: 
- *    Jesus Mena
- *
- * mex ray_mesh_intersect.cpp
- * 
- */
-
-void parse_rhs(
-  int nrhs, 
-  const mxArray *prhs[], 
-  double *& o, 
-  double *& d,
-  double *& V,
-  int & n,
-  double *& F,
-  int & m)
-{
-  mexErrMsgTxt(
-     (mxGetM(prhs[0]) * mxGetN(prhs[0])) == 3, 
-    "Ray origin must be 3d row or column vector");
-  // set origin pointer
-  o = mxGetPr(prhs[0]);
-
-  mexErrMsgTxt(
-     (mxGetM(prhs[1]) * mxGetN(prhs[1])) == 3, 
-    "Ray direction must be 3d row or column vector");
-  // set direction pointer
-  d = mxGetPr(prhs[1]);
-
-  mexErrMsgTxt(mxGetN(prhs[2]) == 3, 
-    "Mesh vertex list must be #V by 3 list of 3D vertex positions");
-  // set number of mesh vertices
-  n = mxGetM(prhs[2]);
-  // set vertex position pointers
-  V = mxGetPr(prhs[2]);
-
-  mexErrMsgTxt(mxGetN(prhs[3]) == 3, 
-    "Mesh face list must be #F by 3 list of triangle indices");
-  // set number of faces
-  m = mxGetM(prhs[3]);
-  // set face index list pointer
-  F = mxGetPr(prhs[3]);
-}
-
-void prepare_lhs(
-  int nlhs,
-  mxArray *plhs[],
-  int m,
-  mxLogical *& flag,
-  double *& t,
-  double *& lambda)
-{
-  // Create pointers for output arrays
-  if(nlhs > 0)
-  {
-    // hit or not
-    plhs[0] = mxCreateLogicalMatrix(m,1);
-    flag = mxGetLogicals(plhs[0]);
-  }
-  if(nlhs > 1)
-  {
-    // distances
-    plhs[1] = mxCreateDoubleMatrix(m,1, mxREAL);
-    t = mxGetPr(plhs[1]);
-  }
-  if(nlhs > 2)
-  {
-    // barycentric coordinates
-    plhs[2] = mxCreateDoubleMatrix(m,3, mxREAL);
-    lambda = mxGetPr(plhs[2]);
-  }
-}
-
-void mexFunction(int nlhs, mxArray *plhs[], 
+void mexFunction(int nlhs, mxArray *plhs[],
     int nrhs, const mxArray *prhs[])
 {
-  mexErrMsgTxt(nrhs == 4, "The number of input arguments must be 4.");
+  // This is useful for debugging whether Matlab is caching the mex binary
+  //mexPrintf("%s %s\n",__TIME__,__DATE__);
+  igl::matlab::MexStream mout;
+  std::streambuf *outbuf = std::cout.rdbuf(&mout);
+  using namespace std;
+  using namespace Eigen;
+  using namespace igl;
+  using namespace igl::matlab;
+  using namespace igl::embree;
+  // Embree uses singles
+  MatrixXf V,source,dir;
+  MatrixXi F;
+  mexErrMsgTxt(nrhs==4,"nrhs should == 4");
 
-  
-  // 3D ray origin
-  double * o;
-  // 3D ray direction
-  double * d;
-  // vertex position list
-  double * V;
-  // face list
-  double * F;
-  // number of mesh vertices
-  int n;
-  // number of mesh faces
-  int m;
-  parse_rhs(nrhs,prhs,o,d,V,n,F,m);
+  parse_rhs_double(prhs+0,source);
+  parse_rhs_double(prhs+1,dir);
+  parse_rhs_double(prhs+2,V);
+  parse_rhs_index( prhs+3,F);
+  mexErrMsgTxt(source.cols()==3,"source must be #source by 3");
+  mexErrMsgTxt(dir.cols()==3,"dir must be #dir by 3");
+  mexErrMsgTxt(source.rows() == dir.rows(), "#source must equal #dir");
+  mexErrMsgTxt(V.cols()==3,"V must be #V by 3");
+  mexErrMsgTxt(F.cols()==3,"F must be #F by 3");
 
-  // #F list of bools whether face intersects ray
-  mxLogical * flag = NULL;
-  // #F list of distances from the ray origin
-  double * t = NULL;
-  // #F list of barycentric coordinate cooresponding to hits on each triangle
-  double * lambda = NULL;
-  prepare_lhs(nlhs,plhs,m,flag,t,lambda);
-  // Just do nothing if no outputs
-  if( flag == NULL)
+  const int n = source.rows();
+  Eigen::Matrix<int, Eigen::Dynamic, 1> id(n);
+  Eigen::Matrix<float, Eigen::Dynamic, 1> t(n);
+  Eigen::Matrix<float, Eigen::Dynamic, 3> lambda(n,3);
+
+  EmbreeIntersector ei;
+  ei.init(V,F,true);
+  for(int si = 0;si<n;si++)
   {
-    return;
+    Eigen::Vector3f s = source.row(si);
+    Eigen::Vector3f d = dir.row(si);
+    igl::Hit hit;
+    const float tnear = 1e-4f;
+    if(ei.intersectRay(s,d,hit,tnear))
+    {
+      id(si) = hit.id;
+      t(si) = hit.t;
+      lambda(si,0) = 1.0-hit.u-hit.v;
+      lambda(si,1) = hit.u;
+      lambda(si,2) = hit.v;
+    }else
+    {
+      id(si) = -1;
+      t(si) = std::limits<float>::infinity;
+      lambda.row(si).setZero();
+    }
   }
 
-  ray_mesh_intersect(o,d,V,F,n,m,flag,t,lambda);
-}
 
+  switch(nlhs)
+  {
+    default:
+    {
+      mexErrMsgTxt(false,"Too many output parameters.");
+    }
+    case 3:
+    {
+      prepare_lhs_double(lambda,plhs+2);
+      // Fall through
+    }
+    case 2:
+    {
+      prepare_lhs_double(t,plhs+1);
+      // Fall through
+    }
+    case 1:
+    {
+      prepare_lhs_index(id,plhs+0);
+      // Fall through
+    }
+    case 0: break;
+  }
+
+  // Restore the std stream buffer Important!
+  std::cout.rdbuf(outbuf);
+}
