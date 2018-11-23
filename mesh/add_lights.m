16de1ad139266181b9ffc6bd86371a21cfa49158
Alec Jacobson <alecjacobson@gmail.com>
2018-11-23 11:10:37 -0500

    documentation

--- mesh/add_lights.m
+++ mesh/add_lights.m
@@ -1,5 +1,11 @@
 function ls = add_lights()
   % ADD_LIGHTS  Add 5 lights  based on the current camera
+  %
+  %ls = add_lights()
+  %
+  % Outputs:
+  %   ls  #ls list of light handles
+  %
 lights = @() { ...
 light('Color',1.0*[1 1 1],'Position',(campos-camtarget),'Style','local'), ...
 light('Color',0.5*teal,'Position', (campos-camtarget)*axisangle2matrix([0 0 1],pi/2),'Style','local'), ...
