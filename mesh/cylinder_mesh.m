0c7a2bbf33689742967b22b1af156299dff57658
Alec Jacobson <alecjacobson@gmail.com>
2020-01-08 00:06:58 -0500

    stacks and caps

--- mesh/cylinder_mesh.m
+++ mesh/cylinder_mesh.m
@@ -8,7 +8,10 @@ function [CV,CF] = cylinder_mesh(R,N,varargin);
   %   R  radius
   %   N number of vertices around axs
   %   Optional:
-  %     'Quads'  whether to output quads instead of triangles
+  %     'Quads'  whether to output quads instead of triangles {false}
+  %     'Caps'  whether to include caps to close the mesh (doesn't apply to
+  %       quads) {false}
+  %     'Stacks' number of face rings on the side of the cylinder {1}
   % Outputs:
   %   CV  #CV by 3 list of mesh vertex positions
   %   CF  #CF by 3 list of mesh triangle indices into rows of CV
@@ -24,10 +27,11 @@ function [CV,CF] = cylinder_mesh(R,N,varargin);
 
   quads = false;
   caps = false;
+  stacks = 1;
   % Map of parameter names to variable names
   params_to_variables = containers.Map( ...
-    {'Caps','Quads'}, ...
-    {'caps','quads'});
+    {'Caps','Quads','Stacks'}, ...
+    {'caps','quads','stacks'});
   v = 1;
   while v <= numel(varargin)
     param_name = varargin{v};
@@ -41,24 +45,34 @@ function [CV,CF] = cylinder_mesh(R,N,varargin);
     end
     v=v+1;
   end
-  [X,Y,Z] = cylinder(R,N);
-  if quads
-    surf2patch_params = {};
-  else
-    surf2patch_params = {'triangles'};
+  CV = [];
+  CF = [];
+  for stack = 1:stacks
+    [X,Y,Z] = cylinder(R,N);
+    if quads
+      surf2patch_params = {};
+    else
+      surf2patch_params = {'triangles'};
+    end
+    [CFs,CVs] = surf2patch(X,Y,Z,surf2patch_params{:});
+    CF = [CF;size(CV,1)+CFs];
+    CV = [CV;CVs+[0 0 stack-1]];
   end
-  [CF,CV] = surf2patch(X,Y,Z,surf2patch_params{:});
-  [CV,~,I] = remove_duplicate_vertices(CV,eps);
-  CF = I(CF);
+
   if caps
+    assert(~quads,'Caps only available for triangles');
     % 13/11/2018
     %CF = [CF;fill_holes(CV,CF)];
     OB = [1:2:2*N      ; 2*N-1 1:2:2*N-3]';
-    OT = [2*N 2:2:2*N-2; 2:2:2*N        ]';
+    OT = 2*(N+1)*(stacks-1)+[2*N 2:2:2*N-2; 2:2:2*N        ]';
     CF = [ ...
       CF; ...
       repmat(size(CV,1)+1,size(OB,1),1) OB; ...
       repmat(size(CV,1)+2,size(OT,1),1) OT];
-    CV = [CV;0 0 0;0 0 1];
+    CV = [CV;0 0 0;0 0 stacks];
   end
+
+  [CV,~,I] = remove_duplicate_vertices(CV,eps);
+  CV = CV./[1 1 stacks];
+  CF = I(CF);
 end
