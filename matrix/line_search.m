d5253129da815426005472f8af11e035f68044d9
Alec Jacobson <alecjacobson@gmail.com>
2020-02-28 14:51:18 -0500

    line search

---
+++ matrix/line_search.m
@@ -0,0 +1,37 @@
+function [step_size,X,obj,step] = line_search(obj_fun,proj_fun,X0,step0,max_step_size)
+  % LINE_SEARCH Given an objective function (obj_fun) to minimize and
+  % constraints to project onto (proj_fun) and an intial guess (X0) determine a
+  % feasible step_size to move along a desired direcxtion (step0)
+  %
+  % [step_size,X,obj,step] = line_search(obj_fun,proj_fun,X0,step0,max_step_size)
+  %
+  % Inputs:
+  %   obj_fun  objective function taking X as input
+  %   proj_fun constraints projection function taking X as input
+  %   X0  #X by 1 list of initial values
+  %   step0  #X by 1 desired step direction
+  %   max_step_size  maximum step size to consider
+  % Outputs:
+  %   step_size  scalar safe step size
+  %   X  #X by 1 new values (X = proj_fun(X0+step_size*step0))
+  %   obj  scalar objective value at X
+  %   step  #X by 1 step conducted before projection (step_size*step0)
+  %
+  
+  step_size = max_step_size;
+  obj0 = obj_fun(proj_fun(X0));
+  for iter = 1:20
+    step = step_size*step0;
+    X = proj_fun(X0+step);
+    obj  = obj_fun(X);
+    if obj < obj0
+      return;
+    else
+      step_size = step_size*0.5;
+    end
+  end
+  step_size = 0;
+  X = X0;
+  obj = obj0;
+  step = zeros(size(step));
+end
