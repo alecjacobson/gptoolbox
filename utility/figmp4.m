58ae20989b721af4a19f4932f12203f121113540
Alec Jacobson <alecjacobson@gmail.com>
2020-04-25 18:47:34 -0400

    figmp4

---
+++ utility/figmp4.m
@@ -0,0 +1,33 @@
+function vobj = figmp4(name,vobj,nf)
+  % vobj = figmp4(name,vobj)
+  %
+  % Inupts:
+  %   name  path to .mp4 file
+  %   vobj  [] on first call and see output for subsequent calls
+  % Outputs:
+  %   vobj  mp4 video object. the video will not be valid until you call
+  %     vobj.close
+  % 
+  % Edit your code in ** three ** places
+  %
+  %   vobj = [];
+  %   for iter = 1:10
+  %      ...
+  %      drawnow;
+  %      vobj = figmp4('myvideo.mp4',vobj);
+  %   end
+  %   vobj.close;
+  %
+  if ~exist('nf','var')
+    nf = 1;
+  end
+  if isempty(vobj)
+    vobj = VideoWriter(name,'MPEG-4');
+    vobj.Quality = 100;
+    vobj.open;
+  end
+  im = getfield(getframe(gcf),'cdata');
+  for f = 1:nf
+    vobj.writeVideo(im);
+  end
+end
