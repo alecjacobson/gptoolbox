46d81d25ec42010280442466a010216a19fe6bcb
Alexandre Mercier-Aubin <alexandremercieraubin@gmail.com>
2021-04-21 00:19:58 -0400

    missing ;

--- mesh/readMESH.m
+++ mesh/readMESH.m
@@ -15,7 +15,7 @@ function [V,T,F] = readMESH( filename )
 
   % First line is mandatory header
   MESHheader = eat_comments(fp,'#');
-  MESHheader = regexprep(MESHheader,'[\n\r]+','')
+  MESHheader = regexprep(MESHheader,'[\n\r]+','');
   if(strcmp(MESHheader,'MeshVersionFormatted 1')==0)
     error('First line should be "MeshVersionFormatted 1" not ("%s")...',MESHheader);
   end
