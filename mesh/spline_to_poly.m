8dc6997b676f540179a54b0da5128da759c120cc
Alec Jacobson <alecjacobson@gmail.com>
2021-05-03 14:00:51 -0400

    id in output

--- mesh/spline_to_poly.m
+++ mesh/spline_to_poly.m
@@ -1,4 +1,4 @@
-function [V,E] = spline_to_poly(P,C,tol)
+function [V,E,I] = spline_to_poly(P,C,tol)
   % SPLINE_TO_POLY Evaluate a cubic Bezier spline as a polyline where each
   % segment corresponds to a locally flat segment of the curve up to given
   % tolerance
@@ -12,15 +12,21 @@ function [V,E] = spline_to_poly(P,C,tol)
   % Outputs:
   %   V  #V by dim list of vertex locations
   %   E  #E by dim list of edge indices into V
+  %   I  #E list of indices into 1:#C
   %
+
   V = P;
   E = [];
+  I = [];
   % consider each cubic
   for c = 1:size(C,1)
     Pc = cubic_flat_eval(P(C(c,:),:),tol);
     J = [C(c,1) size(V,1)+(1:size(Pc,1)-2) C(c,4)];
-    E = [E; [J(1:end-1);J(2:end)]'];
+    Ec = [J(1:end-1);J(2:end)]';
+    E = [E; Ec];
     V = [V;Pc(2:end-1,:)];
+    I = [I;repmat(c,size(Ec,1),1)];
   end
   [V,~,~,E] = remove_unreferenced(V,E);
+
 end
