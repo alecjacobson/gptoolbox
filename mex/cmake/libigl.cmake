33075cdcaf0963648f486b775d44d61535754173
Alec Jacobson <alecjacobson@adobe.com>
2022-02-24 19:21:21 -0500

    no tabs

--- mex/cmake/libigl.cmake
+++ mex/cmake/libigl.cmake
@@ -4,8 +4,8 @@ endif()
 
 include(FetchContent)
 FetchContent_Declare(
-    libigl
-    GIT_REPOSITORY https://github.com/libigl/libigl.git
-		GIT_TAG        v2.4.0
+  libigl
+  GIT_REPOSITORY https://github.com/libigl/libigl.git
+  GIT_TAG v2.4.0
 )
 FetchContent_MakeAvailable(libigl)
