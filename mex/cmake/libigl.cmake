36df062a7ce1b6fab3692c3f0c764f77feed0cc1
Alec Jacobson <alecjacobson@adobe.com>
2022-02-24 19:43:21 -0500

    bump libigl

--- mex/cmake/libigl.cmake
+++ mex/cmake/libigl.cmake
@@ -6,6 +6,6 @@ include(FetchContent)
 FetchContent_Declare(
   libigl
   GIT_REPOSITORY https://github.com/libigl/libigl.git
-  GIT_TAG v2.4.0
+  GIT_TAG fe7e8281a4d92d422d8e516e155c3dd7cd26ab19
 )
 FetchContent_MakeAvailable(libigl)
