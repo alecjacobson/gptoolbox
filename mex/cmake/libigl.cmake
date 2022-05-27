766107b51cc3ee9b763e1ca9a328b3b3e43cc048
Alec Jacobson <alecjacobson@adobe.com>
2022-05-27 00:58:48 -0400

    improve principal curvature mex; igl::exact_geodesic mex

--- mex/cmake/libigl.cmake
+++ mex/cmake/libigl.cmake
@@ -6,6 +6,6 @@ include(FetchContent)
 FetchContent_Declare(
   libigl
   GIT_REPOSITORY https://github.com/libigl/libigl.git
-  GIT_TAG 45a4f79b5ec72327c59758fc87f6fb72a9edffc8
+  GIT_TAG 30019acb17197abb6f3fe4c310909c817f2e85cc
 )
 FetchContent_MakeAvailable(libigl)
