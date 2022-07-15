169da93daf5127f438b4c16f8e3231538ed53dad
Tiago de Souza Fernandes <43066400+Tiagosf00@users.noreply.github.com>
2022-07-15 13:50:14 -0300

    consider steiner points in the barycenter function
    Triangulate may return a vertex list different from the original list given in the input, with new steiner points.

--- mesh/point_inside_polygon.m
+++ mesh/point_inside_polygon.m
@@ -15,8 +15,8 @@ function interior_point = point_inside_polygon(V)
   if exist('triangulate')==3
     % triangulate and pick farthest barycenter from polygon boundary
     E = [1:size(V,1);2:size(V,1) 1]';
-    [~,F] = triangulate(V,E);
-    BC = barycenter(V,F);
+    [Vt,Ft] = triangulate(V,E);
+    BC = barycenter(Vt,Ft);
     D = point_mesh_squared_distance(BC,V,E);
     [~,m] = max(D);
     interior_point = BC(m,:);
