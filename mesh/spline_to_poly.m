7f5ee83fc44531b6c3be402826abe7def1e04861
Alec Jacobson <alecjacobson@adobe.com>
2022-09-19 23:42:32 -0400

    minor speedup

--- mesh/spline_to_poly.m
+++ mesh/spline_to_poly.m
@@ -15,18 +15,22 @@ function [V,E,I] = spline_to_poly(P,C,tol)
   %   I  #E list of indices into 1:#C
   %
 
-  V = P;
+  % use transpose for amortized concatenation...
+  V = P';
   E = [];
   I = [];
   % consider each cubic
   for c = 1:size(C,1)
     Pc = cubic_flat_eval(P(C(c,:),:),tol);
-    J = [C(c,1) size(V,1)+(1:size(Pc,1)-2) C(c,4)];
+    J = [C(c,1) size(V,2)+(1:size(Pc,1)-2) C(c,4)];
     Ec = [J(1:end-1);J(2:end)]';
-    E = [E; Ec];
-    V = [V;Pc(2:end-1,:)];
+    % use transpose for amortized concatenation...
+    E = [E,                    Ec'];
+    V = [V,         Pc(2:end-1,:)'];
     I = [I;repmat(c,size(Ec,1),1)];
   end
+  V = V';
+  E = E';
   [V,~,~,E] = remove_unreferenced(V,E);
 
 end
