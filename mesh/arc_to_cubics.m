423f78bb027d62db35c06f1f66ce685ba5472a7c
Alec Jacobson <alecjacobson@adobe.com>
2023-06-18 11:50:05 -0400

    degenerate arcs, weird svg: prefix

--- mesh/arc_to_cubics.m
+++ mesh/arc_to_cubics.m
@@ -14,10 +14,18 @@ function [Pe,Ce] = arc_to_cubics(Pabs,Pnext,rx,ry,phi,large_arc,sweep)
   %   Ce  #Ce by 4 list of cubic bezier indices into rows of Pe
   %
 
+  if rx == 0 || ry == 0
+    warning('arc_to_cubics:zero-radius rx or ry is zero');
+    Pe = zeros(0,2);
+    Ce = zeros(0,4);
+    return;
+  end
+
   phi_rad = phi*pi/180;
   R = [cos(phi_rad) -sin(phi_rad) 0;sin(phi_rad) cos(phi_rad) 0;0 0 1];
   S = diag([1 rx/ry 1]);
   T = [eye(2,3);-Pabs 1] * R * S;
+
   [Pe,Ce] = circular_arc_to_cubics([Pnext 1]*T(:,1:2),rx,large_arc,sweep);
   Pe = [Pe ones(size(Pe,1),1)] * inv(T);
   Pe = Pe(:,1:2);
