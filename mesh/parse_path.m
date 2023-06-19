e5cbc7a97f2a9334628371c935054fb72a91cc63
Alec Jacobson <alecjacobson@adobe.com>
2023-06-19 17:44:15 -0400

    remove O(n^2) perf issues

--- mesh/parse_path.m
+++ mesh/parse_path.m
@@ -16,54 +16,178 @@ function [P,C,state] = parse_path(dstr,varargin)
   %
 
 
-  % These are all annoyingly O(#dstr) 
-  function [x,dstr] = parse_x(dstr)
-    [x,count,~,pos] = sscanf(dstr,'%g',1);
+  % These should now be output sensitive (i.e., O(m) not O(n) where m is
+  % position of final character needed to parse and n is length of dstr)
+  function [x,pos] = parse_x(dstr,pos)
+    [x,count,~,pos_off] = sscanf(dstr(pos:end),'%g',1);
     if count~= 1
       x = [];
       return;
     end
-    dstr = dstr(pos:end);
-    dstr = strip(strip(dstr,'left',','),'left',' ');
+    pos = pos + pos_off - 1;
+    % Eat spaces and commas
+    while pos <= length(dstr) && (dstr(pos) == ',' || dstr(pos) == ' '); pos = pos + 1; end
   end
-  function [x,dstr] = parse_flag(dstr)
-    pos = 1;
-    % eat delimiters
-    while pos <= length(dstr) && ...
-        (dstr(pos) == ' ' || dstr(pos) == ',')
-      pos = pos + 1;
-    end
+  function [x,pos] = parse_flag(dstr,pos)
+    % Eat spaces and commas
+    while pos <= length(dstr) && (dstr(pos) == ',' || dstr(pos) == ' '); pos = pos + 1; end
     if dstr(pos) == '0' || dstr(pos) == '1'
       x = dstr(pos) == '1';
       pos = pos + 1;
     else
       x = [];
     end
-    dstr = dstr(pos:end);
-    dstr = strip(strip(dstr,'left',','),'left',' ');
+    % Eat spaces and commas
+    while pos <= length(dstr) && (dstr(pos) == ',' || dstr(pos) == ' '); pos = pos + 1; end
   end
-  function [xy,dstr] = parse_xy(dstr)
-    [x,dstr] = parse_x(dstr);
-    [y,dstr] = parse_x(dstr);
+  function [xy,pos] = parse_xy(dstr,pos)
+    [x,pos] = parse_x(dstr,pos);
+    [y,pos] = parse_x(dstr,pos);
     if isempty(y) 
       xy = [];
       return;
     end
     xy = [x;y];
   end
-  function [key,dstr] = parse_key(dstr)
-    % only support open cubic Bezier splines for now
-    keys = 'CcHhLlMmSsVvZzQqAaTt';
-    [key,count,~,pos] = sscanf(dstr,'%c',1);
-    if count~= 1 
-      key = [];
-    elseif ~ismember(key,keys)
-      key = [];
-    else
-      dstr = dstr(pos:end);
+
+  % Annoying way to effectively impement a class.
+  function [state,pos] = parse_A(state,dstr,pos)
+    [rx, pos] = parse_x(dstr,pos);
+    [ry, pos] = parse_x(dstr,pos);
+    [phi,pos] = parse_x(dstr,pos);
+    % it seems valid for the flags to appear immediately adjacent to each
+    % other like '10' or '11' which should be read as '1 0' and '1 1' rather
+    % than 10→1 and 11→1
+    % 
+    % Another file ('222268.svg') appeared to use '1,11' to mean '1 1' but I
+    % can't think of a way to correctly handle both uses.
+    [large_arc,pos] = parse_flag(dstr,pos);
+    [sweep,pos] = parse_flag(dstr,pos);
+    [Pnext,pos] = parse_xy(dstr,pos);
+    if isempty(Pnext)
+      error('parse_path: wrong number of args for A,a');
+    end
+
+    Pnext = (state.key=='a')*state.Pabs + Pnext;
+
+    if ~isequal(state.Pabs,Pnext)
+      [Pe,Ce] = arc_to_cubics(state.Pabs',Pnext',rx,ry,phi,large_arc,sweep);
+
+      if ~isempty(Ce)
+        state.C = [state.C size(state.P,2)+Ce'-1];
+        state.P = [state.P Pe(2:end,:)'];
+        state.Pabs = state.P(:,end);
+      end
     end
   end
 
+
+  function [state,pos] = parse_C(state,dstr,pos)
+    state.C = [state.C size(state.P,2)+[0 1 2 3]'];
+    [xy,pos] = parse_xy(dstr,pos);
+    state.P(:,end+1) = xy;
+    [xy,pos] = parse_xy(dstr,pos);
+    state.P(:,end+1) = xy;
+    [xy,pos] = parse_xy(dstr,pos);
+    state.P(:,end+1) = xy;
+
+    if state.key == 'c'
+      state.P(:,end-2:end) = state.P(:,end-2:end) + state.Pabs;
+    end
+    state.Pabs = state.P(:,end);
+  end
+
+  function [state,pos] = parse_L(state,dstr,pos)
+      % augh I hate that I'm using epsilon here. probably the interp1 above is
+      % leading to small numerical noise.
+      if (state.key == 'Z' || state.key == 'z') && size(state.P,2)==state.mi
+        % degenerate single point, ignore
+      elseif (state.key == 'Z' || state.key == 'z') && sum((state.P(:,end)-state.P(:,state.mi)).^2)<eps
+        % close up naturally by identifying first and last point
+        if ~isempty(state.C)
+          state.C(4,end) = state.mi;
+        end
+        % Pop last point
+        state.P =    state.P(:,1:end-1);
+        state.Pabs = state.P(:,state.mi);
+      else
+        state.C = [state.C size(state.P,2)+[0 1 2 3]'];
+        switch state.key
+        case {'L','l'}
+          [XY,pos] = parse_xy(dstr,pos);
+          Pnext = (state.key=='l')*state.Pabs + XY;
+        case {'V','v'}
+          [Y,pos] = parse_x(dstr,pos);
+          Pnext = [1;(state.key=='v')].*state.Pabs + [0;Y];
+        case {'H','h'}
+          [X,pos] = parse_x(dstr,pos);
+          Pnext = [(state.key=='h');1].*state.Pabs + [X;0];
+        case {'Z','z'}
+          % I don't see how Z or z are different.
+          % Don't parse xy, connect to beginning 
+          Pnext = state.P(:,state.mi);
+        end
+        state.P(:,end+1) = state.Pabs + (1/3)*(Pnext-state.Pabs);
+        state.P(:,end+1) = state.Pabs + (2/3)*(Pnext-state.Pabs);
+        state.P(:,end+1) = Pnext;
+        state.Pabs = state.P(:,end);
+      end
+  end
+
+  function [state,pos] = parse_M(state,dstr,pos)
+    [Pnext,pos] = parse_xy(dstr,pos);
+    Pm = Pnext+(state.key=='m')*state.Pabs;
+    state.P = [state.P Pm];
+    state.mi = size(state.P,2);
+    state.Pabs = state.P(:,end);
+    % Augh this can even happen after 'mz', set key here so below state.prev_key is
+    % correct.
+    state.key = char('L' + (state.key-'M'));
+  end
+
+  function [state,pos] = parse_S(state,dstr,pos)
+    state.C = [state.C size(state.P,2)+[0 1 2 3]'];
+    if ismember(state.prev_key,'SsCc')
+      state.P(:,end+1) = state.P(:,end)+ state.P(:,end)-state.P(:,end-1);
+    else
+      state.P(:,end+1) = state.P(:,end);
+    end
+    [state.P(:,end+1),pos] = parse_xy(dstr,pos);
+    [state.P(:,end+1),pos] = parse_xy(dstr,pos);
+    if state.key == 's'
+      state.P(:,end-1:end) = state.P(:,end-1:end) + state.Pabs;
+    end
+    state.Pabs = state.P(:,end);
+  end
+
+  function [state,pos] = parse_Q(state,dstr,pos)
+      Q1 = state.Pabs;
+      [Q2,pos] = parse_xy(dstr,pos);
+      [Q3,pos] = parse_xy(dstr,pos);
+      Q = [Q1 [Q2 Q3] + (state.key=='q')*state.Pabs]';
+      CQ = quadratic_to_cubic(Q);
+      state.C = [state.C size(state.P,2)+[0 1 2 3]'];
+      state.P = [state.P CQ(2:end,:)'];
+
+      state.Qprev = Q(2,:)';
+      state.Pabs = state.P(:,end);
+  end
+  function [state,pos] = parse_T(state,dstr,pos)
+      Q1 = state.Pabs;
+      if ismember(state.prev_key,'QqTt')
+        Q2 = state.Pabs + (state.Pabs-state.Qprev);
+      else
+        Q2 = state.Pabs;
+      end
+      [Q3,pos] = parse_xy(dstr,pos);
+      Q3 = Q3 + (state.key=='t')*state.Pabs;
+      Q = [Q1 Q2 Q3]';
+      CQ = quadratic_to_cubic(Q);
+      state.C = [state.C size(state.P,2)+[0 1 2 3]'];
+      state.P = [state.P CQ(2:end,:)'];
+      state.Qprev = Q(2,:);
+      state.Pabs = state.P(:,end);
+  end
 
   data = [];
   state_Pabs = [0 0];
@@ -97,20 +221,21 @@ function [P,C,state] = parse_path(dstr,varargin)
     % Split into sub-paths
     pattern = '([Mm][^Mm]*)';
     S = regexp(dstr, pattern, 'match');
-    P = [];
-    C = [];
+    state.P = [];
+    state.C = [];
     state.Pabs = [0 0];
     for i = 1:length(S)
-      [Pi,Ci,state] = parse_path(S{i},'Cursor',state.Pabs,'Split',false);
-      C = [C size(P,2)+Ci'];
-      P = [P Pi'];
+      [Pi,Ci,state_i] = parse_path(S{i},'Cursor',state.Pabs,'Split',false);
+      state.Pabs = state_i.Pabs;
+      state.C = [state.C size(state.P,2)+Ci'];
+      state.P = [state.P Pi'];
     end
-    P = P';
-    C = C';
+    P = state.P';
+    C = state.C';
     return;
   end
 
-
+  % Combine these somehow?
   dstr = strrep(dstr,',',' ');
   dstr = regexprep(dstr,'(&#x[A-z0-9];)+',' ');
   dstr = regexprep(dstr,'\s+',' ');
@@ -121,186 +246,63 @@ function [P,C,state] = parse_path(dstr,varargin)
     return;
   end
 
-  [state.key,dstr] = parse_key(strtrim(dstr));
-  assert(state.key == 'M' || state.key == 'm','First key must be M or m');
-  [P,dstr] = parse_xy(dstr);
-  P = (state.key == 'm')*state.Pabs + P;
-  state.mi = size(P,2);
-  % state.Pabs will track the current cursor position
-  state.Pabs = P(:,end);
-  C = [];
-  state.z_seen = false;
-  % I guess L is some kind of default...
-  state.prev_key = char('L' + (state.key-'M'));
+  keys = 'CcHhLlMmSsVvZzQqAaTt';
+  pattern = sprintf('([%s][^%s]*)',keys,keys);
+  S = regexp(dstr, pattern, 'match');
+  if isempty(S)
+    P = zeros(0,2);
+    C = zeros(0,4);
+    return;
+  end
+
+  state.P = zeros(2,0);
+  state.C = zeros(4,0);
+  state.key = S{1}(1);
   state.Qprev = [];
-  while ~isempty(dstr)
+  assert(state.key == 'M' || state.key == 'm','First key must be M or m');
+  for si = 1:numel(S)
+    dstr_i = S{si};
+    state.key = dstr_i(1);
+    pos = 2;
+    % Since these are each copying and editing dstr_i, this still incurs a O(n²)
+    % performance hit, where n is the length of a command (or chain of commands
+    % with same key [potentially very long]).
+    %
+    % A finally optimization would be to be careful to not copy/edit dstr_i or
+    % to do an initial pass to break up chains of commands.
+    %
+    % Currently, I think it would be easier to maintain dstr_i and pos and
+    % change each parse_ command to increment on pos without altering dstr_i
+    while true
+      switch(state.key)
+      case {'A','a'}
+        [state,pos] = parse_A(state,dstr_i,pos);
+      case {'C','c'}
+        [state,pos] = parse_C(state,dstr_i,pos);
+      case {'L','l','V','v','H','h','Z','z'}
+        [state,pos] = parse_L(state,dstr_i,pos);
+      case {'M','m'}
+        [state,pos] = parse_M(state,dstr_i,pos);
+      case {'Q','q'}
+        [state,pos] = parse_Q(state,dstr_i,pos);
+      case {'S','s'}
+        [state,pos] = parse_S(state,dstr_i,pos);
+      case {'T','t'}
+        [state,pos] = parse_T(state,dstr_i,pos);
+      otherwise
+        error('%c key not supported',state.key)
+      end
 
-    % State:
-    %   state.prev_key
-    %   state.z_seen
-    %   state.Pabs
-    %   state.Qprev
-    %   state.mi
-    % 
-    %   P
-    %   C
-
-    [state.key,dstr] = parse_key(dstr);
-
-    % A command letter may be elistate.minated if an identical command letter would
-    % otherwise precede it; for instance, the following contains an unnecessary
-    % second "L" command:
-    if isempty(state.key) && ~isempty(state.prev_key)
-      state.key = state.prev_key;
+      % would like to get rid of this:
+      while pos <= length(dstr_i) && (dstr_i(pos) == ',' || dstr_i(pos) == ' '); pos = pos + 1; end
+      if pos>length(dstr_i)
+        break;
+      end
     end
-    switch state.key
-    case {'A','a'}
-
-      %% Handle full circles made of two 180° arcs as a special case.
-      %if key == 'a'
-      %end
-
-      %[Z,count,~,pos] = sscanf(dstr,'%g',7);
-      %assert(count == 7,'Expected 7 values for A,a command');
-      %% O(n) :-(
-      %dstr = strtrim(dstr(pos:end));
-      [rx,dstr] = parse_x(dstr);
-      [ry,dstr] = parse_x(dstr);
-      [phi,dstr] = parse_x(dstr);
-      % it seems valid for the flags to appear immediately adjacent to each
-      % other like '10' or '11' which should be read as '1 0' and '1 1' rather
-      % than 10→1 and 11→1
-      % 
-      % Another file ('222268.svg') appeared to use '1,11' to mean '1 1' but I
-      % can't think of a way to correctly handle both uses.
-      [large_arc,dstr] = parse_flag(dstr);
-      [sweep,dstr] = parse_flag(dstr);
-      [Pnext,dstr] = parse_xy(dstr);
-      if isempty(Pnext)
-        error('parse_path: wrong number of args for A,a');
-      end
-
-      Pnext = (state.key=='a')*state.Pabs + Pnext;
-
-      if ~isequal(state.Pabs,Pnext)
-        [Pe,Ce] = arc_to_cubics(state.Pabs',Pnext',rx,ry,phi,large_arc,sweep);
-
-        if ~isempty(Ce)
-          C = [C size(P,2)+Ce'-1];
-          P = [P Pe(2:end,:)'];
-          state.Pabs = P(:,end);
-        end
-      end
-    case {'T','t'}
-      Q1 = state.Pabs;
-      if ismember(state.prev_key,'QqTt')
-        Q2 = state.Pabs + (state.Pabs-state.Qprev);
-      else
-        Q2 = state.Pabs;
-      end
-      [Q3,dstr] = parse_xy(dstr);
-      Q3 = Q3 + (state.key=='t')*state.Pabs;
-      Q = [Q1 Q2 Q3]';
-      CQ = quadratic_to_cubic(Q);
-      C = [C size(P,2)+[0 1 2 3]'];
-      P = [P CQ(2:end,:)'];
-      state.Qprev = Q(2,:);
-      state.Pabs = P(:,end);
-
-    case {'Q','q'}
-      Q1 = state.Pabs;
-      [Q2,dstr] = parse_xy(dstr);
-      [Q3,dstr] = parse_xy(dstr);
-      Q = [Q1 [Q2 Q3] + (state.key=='q')*state.Pabs]';
-      CQ = quadratic_to_cubic(Q);
-      C = [C size(P,2)+[0 1 2 3]'];
-      P = [P CQ(2:end,:)'];
-
-      state.Qprev = Q(2,:)';
-      state.Pabs = P(:,end);
-
-    case {'C','c'}
-      C = [C size(P,2)+[0 1 2 3]'];
-      [P(:,end+1),dstr] = parse_xy(dstr);
-      [P(:,end+1),dstr] = parse_xy(dstr);
-      [P(:,end+1),dstr] = parse_xy(dstr);
-      if state.key == 'c'
-        P(:,end-2:end) = P(:,end-2:end) + state.Pabs;
-      end
-      state.Pabs = P(:,end);
-    case {'L','l','V','v','H','h','Z','z'}
-      if (state.key == 'Z' || state.key == 'z')
-        %assert(isempty(dstr),'Z Should be last key');
-        % gobble white space
-        dstr = strip(dstr);
-      end
-      % augh I hate that I'm using epsilon here. probably the interp1 above is
-      % leading to small numerical noise.
-      if (state.key == 'Z' || state.key == 'z') && size(P,2)==state.mi
-        % degenerate single point, ignore
-      elseif (state.key == 'Z' || state.key == 'z') && sum((P(:,end)-P(:,state.mi)).^2)<eps
-        % close up naturally by identifying first and last point
-        if ~isempty(C)
-          C(4,end) = state.mi;
-        end
-        % Pop last point
-        P = P(:,1:end-1);
-        state.Pabs = P(:,state.mi);
-      else
-        C = [C size(P,2)+[0 1 2 3]'];
-        switch state.key
-        case {'L','l'}
-          [XY,dstr] = parse_xy(dstr);
-          Pnext = (state.key=='l')*state.Pabs + XY;
-        case {'V','v'}
-          [Y,dstr] = parse_x(dstr);
-          Pnext = [1;(state.key=='v')].*state.Pabs + [0;Y];
-        case {'H','h'}
-          [X,dstr] = parse_x(dstr);
-          Pnext = [(state.key=='h');1].*state.Pabs + [X;0];
-        case {'Z','z'}
-          % I don't see how Z or z are different.
-          % Don't parse xy, connect to beginning 
-          Pnext = P(:,state.mi);
-        end
-        P(:,end+1) = state.Pabs + (1/3)*(Pnext-state.Pabs);
-        P(:,end+1) = state.Pabs + (2/3)*(Pnext-state.Pabs);
-        P(:,end+1) = Pnext;
-        state.Pabs = P(:,end);
-      end
-    case {'S','s'}
-      C = [C size(P,2)+[0 1 2 3]'];
-      if ismember(state.prev_key,'SsCc')
-        P(:,end+1) = P(:,end)+ P(:,end)-P(:,end-1);
-      else
-        P(:,end+1) = P(:,end);
-      end
-      [P(:,end+1),dstr] = parse_xy(dstr);
-      [P(:,end+1),dstr] = parse_xy(dstr);
-      if state.key == 's'
-        P(:,end-1:end) = P(:,end-1:end) + state.Pabs;
-      end
-      state.Pabs = P(:,end);
-    case {'M','m'}
-      [Pnext,dstr] = parse_xy(dstr);
-      Pm = Pnext+(state.key=='m')*state.Pabs;
-      P = [P Pm];
-      state.mi = size(P,2);
-      state.Pabs = P(:,end);
-      % Augh this can even happen after 'mz', set key here so below state.prev_key is
-      % correct.
-      state.key = char('L' + (state.key-'M'));
-    otherwise
-      error('%c key not supported',state.key)
-    end
-    %key
-    %clf;hold on;arrayfun(@(c) set(plot_cubic(P(:,C(c),:)),'Color','b'),1:size(C,1));hold off;
-    %pause
     state.prev_key = state.key;
   end
 
-  P = P';
-  C = C';
-    
 
+  P = state.P';
+  C = state.C';
 end
