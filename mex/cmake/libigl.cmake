45a161498217c532275d1f2060c1931120874b4b
Alec Jacobson <alecjacobson@gmail.com>
2023-08-20 17:01:35 -0400

    bump to v2.5.0

--- mex/cmake/libigl.cmake
+++ mex/cmake/libigl.cmake
@@ -6,6 +6,6 @@ include(FetchContent)
 FetchContent_Declare(
   libigl
   GIT_REPOSITORY https://github.com/libigl/libigl.git
-  GIT_TAG 3ea7f9480967fcf6bf02ce9b993c0ea6d2fc45f6
+  GIT_TAG 9162fb7d797c2a55ce15e467dcd36e6d9e8ab245
 )
 FetchContent_MakeAvailable(libigl)
