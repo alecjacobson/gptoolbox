09c4cf4ef9a7465a48bd463e866317c13f33a9f3
Alec Jacobson <alecjacobson@adobe.com>
2024-01-22 16:31:13 -0500

    attempt to save split_nonmanifold

--- mesh/split_nonmanifold.m
+++ mesh/split_nonmanifold.m
@@ -35,12 +35,38 @@ function [SF,SVI,SV,A] = split_nonmanifold(F,varargin)
   end
 
 
+  % As if every corner became a vertex
   SF = reshape(1:numel(F),size(F));
+  % These are edges between separated corners
   E = [SF(:,[2 3]);SF(:,[3 1]);SF(:,[1 2])];
-  [I,J] = ismember(F(E),fliplr(F(E)),'rows');
+  % F(E) is just the same as [F(:,[2 3]);F(:,[3 1]);F(:,[1 2])]
+  FE = F(E);
+  FE
+  % I(i) true indicates that the i-th edge in the original connected mesh has
+  % found its consistently oriented twin
+  [I,J] = ismember(FE,fliplr(FE),'rows');
+  % E(I,:) Are edges between corners that have found their consistently oriented
+  % twin in the original mesh
+
+  % I think the idea is to connect faces which share compatibly oriented edges.
+  % But this means for a non-manifold edge with 3 edges e,d,f we'd have e→d, e→f
+  % but not d→f (w.l.o.g).
+
+  % But I think ismember is just finding the 'first' occurrence.
+
+  % This is just getting lucky!
+  warning('this is bugged.');
+
   A = sparse(E(I,:),fliplr(E(J(I),:)),1,numel(F),numel(F));
-  %[~,K] = conncomp(A&A');
-  [~,K] = conncomp(A);
+  %[~,K] = conncomp(A);
+  [~,K] = conncomp(A&A');
+
+  [AI,AJ] = find(A);
+  FE
+  [AI AJ]
+  fprintf('%d,%d → %d,%d\n',[FE(AI,:) FE(AJ,:)]');
+  full(A)
+  
 
   SVI = F(:);
   SV = V(SVI,:);
@@ -52,5 +78,9 @@ function [SF,SVI,SV,A] = split_nonmanifold(F,varargin)
   [SV,IM,J] = remove_unreferenced(SV,SF);
   SF = J(SF);
   SVI = SVI(J);
+
+  %if max(connected_components(SF)) == 1
+  %keyboard
+  %end
 
 end
