0d2d6b5277dabf2762b20ddf0ed1af8ade97e602
Alec Jacobson <alecjacobson@gmail.com>
2025-06-23 16:56:09 -0400

    interp args

--- mesh/apply_texture_map.m
+++ mesh/apply_texture_map.m
@@ -1,4 +1,4 @@
-function [IO,A] = apply_texture_map(tsh,UV,T)
+function [IO,A] = apply_texture_map(tsh,UV,T,varargin);
   % [IO,A] = apply_texture_map(tsh,UV,T)
   % 
   % Inputs:
@@ -15,6 +15,24 @@ function [IO,A] = apply_texture_map(tsh,UV,T)
   %   square. This is to facilitate use with bwmesh.  If reading .obj + .png,
   %   this should be adapted and possibly controled with a flag.
   %
+  interp_args = {};
+  % Map of parameter names to variable names
+  params_to_variables = containers.Map( ...
+    {'InterpArgs'}, ...
+    {'interp_args'});
+  v = 1;
+  while v <= numel(varargin)
+    param_name = varargin{v};
+    if isKey(params_to_variables,param_name)
+      assert(v+1<=numel(varargin));
+      v = v+1;
+      % Trick: use feval on anonymous function to use assignin to this workspace
+      feval(@()assignin('caller',params_to_variables(param_name),varargin{v}));
+    else
+      error('Unsupported parameter: %s',varargin{v});
+    end
+    v=v+1;
+  end
 
   [I,A,FI,B] = shader(tsh);
   [II,~,IV] = find(FI(:));
@@ -27,9 +45,9 @@ function [IO,A] = apply_texture_map(tsh,UV,T)
   [XT,YT] = meshgrid(1:size(T,2),size(T,1):-1:1);
   IT = [];
   T = im2double(T);
-  IT(:,:,1) = interp2(XT,YT,T(:,:,1),PUV(:,:,1),PUV(:,:,2)).*A;
-  IT(:,:,2) = interp2(XT,YT,T(:,:,2),PUV(:,:,1),PUV(:,:,2)).*A;
-  IT(:,:,3) = interp2(XT,YT,T(:,:,3),PUV(:,:,1),PUV(:,:,2)).*A;
+  IT(:,:,1) = interp2(XT,YT,T(:,:,1),PUV(:,:,1),PUV(:,:,2),interp_args{:}).*A;
+  IT(:,:,2) = interp2(XT,YT,T(:,:,2),PUV(:,:,1),PUV(:,:,2),interp_args{:}).*A;
+  IT(:,:,3) = interp2(XT,YT,T(:,:,3),PUV(:,:,1),PUV(:,:,2),interp_args{:}).*A;
   IT(isnan(IT(:))) = 0;
   IO = IT.*A + I.*~A;
 end
