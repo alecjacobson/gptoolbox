6c8dbb11ae42309b86d67e5b408615c56e75e564
Alec Jacobson <alecjacobson@gmail.com>
2025-06-23 16:53:02 -0400

    example

--- imageprocessing/okloop.m
+++ imageprocessing/okloop.m
@@ -11,6 +11,9 @@ function [RGB,OK] = okloop(nc, arc, shift)
   %   n = 15;
   %   X = okloop(n);
   %   X = X(mod((1:floor(n/2):n*floor(n/2)) -1,n)+1,:);
+  %
+  %   % similar hue range as jet
+  %   okloop(256,-4/3*pi,-1/2*pi);
   if nargin < 1, nc = 256; end
   if nargin < 2, arc = 2*pi; end
   if nargin < 3, shift = 0; end
