6b675e8f0d2ef48ebbe4796ad791309206334213
artemyev <a.e.artemyev.work@yandex.ru>
2025-07-04 10:33:53 +0700

    fix(vertex_triangle_adjacency): fix documentation inconsistency

--- mesh/vertex_triangle_adjacency.m
+++ mesh/vertex_triangle_adjacency.m
@@ -7,7 +7,7 @@ function [VT] = vertex_triangle_adjacency(F)
   % Input:
   %   F   #F x 3   list of face indices
   % Output:
-  %   VT  #F x #V  sparse matrix, VT(i,j) is 1 iff vertex i is in face j
+  %   VT  #F x #V  sparse matrix, VT(i,j) is 1 iff vertex j is in face i
 
   i = (1:size(F,1))';
   j = F;
