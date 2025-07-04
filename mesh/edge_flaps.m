9cc7b1cc2b55eed25553fa8a8c277e17613147d0
artemyev <a.e.artemyev.work@yandex.ru>
2025-07-04 10:17:52 +0700

    feat(edge_flaps): EMAP now has sizes of F

--- mesh/edge_flaps.m
+++ mesh/edge_flaps.m
@@ -9,12 +9,13 @@ function [EF,EI,uE,EMAP] = edge_flaps(F)
   %     e=(j->i)
   %   EI  #E by 2 list of edge flap corners (see above).
   %   uE  #uE by 2 list of edge indices into V.
-  %   EMAP #F*3 list of indices into uE, mapping each directed edge to unique
+  %   EMAP #F by 3 list of indices into uE, mapping each directed edge to unique
   %     unique edge in uE
   %
-  E = [F(:,2:3);F(:,[3 1]);,F(:,1:2)];
+  E = [F(:,2:3);F(:,[3 1]);F(:,1:2)];
   sE = sort(E,2);
   [uE,~,EMAP] = unique(sE,'rows');
+  EMAP = reshape(EMAP,size(F));
   I = (1:size(uE,1))';
   [B1,E1] = ismember(uE,E,'rows');
   [B2,E2] = ismember(uE,fliplr(E),'rows');
