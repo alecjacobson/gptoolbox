952051f3f159c57b2d6d5ffb0a74b36a4a9c98f2
artemyev <a.e.artemyev.work@yandex.ru>
2025-07-14 08:33:05 +0700

    fix(triangle_triangle_adjacency): fix Fi in CyclicIndexing

--- mesh/triangle_triangle_adjacency.m
+++ mesh/triangle_triangle_adjacency.m
@@ -95,6 +95,7 @@ function [Fp, Fi] = triangle_triangle_adjacency(F, varargin)
   if strcmpi(mode, 'CyclicIndexing')
     Fp = [Fp(:,3), Fp(:,1), Fp(:,2)];
     Fi = [Fi(:,3), Fi(:,1), Fi(:,2)];
+    Fi = mod(Fi,3)+1;
   elseif ~strcmpi(mode, 'CornerIndexing')
     error('Unknown indexing mode. Use ''CornerIndexing'' or ''CyclicIndexing''.');
   end
