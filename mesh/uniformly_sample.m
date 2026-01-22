99d4aa4c8adebd31dc9dd04e354e12836a9c8549
Alec Jacobson <alecjacobson@gmail.com>
2026-01-22 12:36:20 -0500

    sample ve

--- mesh/uniformly_sample.m
+++ mesh/uniformly_sample.m
@@ -12,6 +12,9 @@ function [NV,NI] = uniformly_sample(V,un,varargin)
   %      % Equivalent to:
   %      NP = uniformly_sample(P([1:end 1],:),100);
   %      NP = NP(1:end-1,:);
+  %    'E' followed by #E by 2 list of edges into V. In this case
+  %    uniformly_sample will be run on each manifold patch. distributing un
+  %    according to length of patch. Loop will be set accordingly
   % Outputs:
   %  NV  un by dim list of ordered outputs points on an open curve
   %  NI  un list of indices into NP of previous node w.r.t. arc length
@@ -20,12 +23,17 @@ function [NV,NI] = uniformly_sample(V,un,varargin)
 
   loop = false;
   v = 1;
+  E = [];
   while v<=numel(varargin)
     switch varargin{v}
     case 'Loop'
       v = v+1;
       assert(v<=numel(varargin));
       loop = varargin{v};
+    case 'E'
+      v = v+1;
+      assert(v<=numel(varargin));
+      E = varargin{v};
     otherwise
       error('Unsupported parameter "%s"',varargin{v});
     end
@@ -34,6 +42,31 @@ function [NV,NI] = uniformly_sample(V,un,varargin)
 
   if loop
     V = V([1:end 1],:);
+  end
+
+  if ~isempty(E)
+    l = edge_lengths(V,E);
+    totl = sum(l);
+    [C] = manifold_patches(E);
+    lC = accumarray(C(:),l);
+    un_C = round(un*(lC./totl));
+    % sum(un_C) not necessarily equal to un due to rounding
+    S = sparse(1:size(E,1),C,1,size(E,1),max(C));
+    NV = [];
+    for c = 1:max(C)
+      Ic = find(S(:,c));
+      Ec = E(Ic,:);
+      % Extract submesh. This next line is now O(|Ic|)
+      [Vc,~,~,Ec] = remove_unreferenced(V,Ec,true);
+      [IIc,J,K] = edges_to_path(Ec);
+      loop_c = IIc(1) == IIc(end);
+      [NVc,NIc] = uniformly_sample(Vc(IIc,:),un_C(c),'Loop',loop_c);
+      % Not figuring these out right now.
+      NIc(:) = nan;
+      NV = [NV NVc'];
+    end
+    NV = NV';
+    return;
   end
 
 
